98bd6456abcbb9aebdbb87ecf15433a5fd0a5f4a
Oleg <admin@MacBook-Pro-Admin.local>
2023-09-22 14:00:19 +0700

    remove console.log

--- build_modules/webpack.js
+++ build_modules/webpack.js
@@ -1,5 +1,4 @@
 const path  = require('path');
-console.log(path.resolve(__dirname))
 const SpriteLoaderPlugin = require("svg-sprite-loader/plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
