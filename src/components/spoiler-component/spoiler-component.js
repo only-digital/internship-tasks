6071fb442cce720682ceed0492294f8022569fb4
AnnaKhiz <kukurika.anna@gmail.com>
2023-09-22 14:29:23 +0300

    added block to pug

---
+++ src/components/spoiler-component/spoiler-component.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/Component';
+
+class SpoilerComponent extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default SpoilerComponent
