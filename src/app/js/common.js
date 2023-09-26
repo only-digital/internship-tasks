f1c5651a0f0431850000419dd4ebedf4bbf13ec3
ShaggyBa <71100001+ShaggyBa@users.noreply.github.com>
2023-09-26 15:45:06 +0300

    create form component

--- src/app/js/common.js
+++ src/app/js/common.js
@@ -1,4 +1,5 @@
 import ExampleButton from "../../components/example-button/example-button";
+import Form from "../../components/form/form";
 
 /**
  * Объект со всеми компонентами, для которых будет применяться автоматическая инициализация
@@ -6,25 +7,26 @@ import ExampleButton from "../../components/example-button/example-button";
  * Значение - JS-класс компонента (Импорт добавляется вручную)
  */
 const allComponents = {
-    'example-button': ExampleButton,
+	'example-button': ExampleButton,
+	"form": Form
 }
 
 /**
  * Инициализация всех компонентов на странице
  */
 try {
-    const existedComponents = Array.from(document.querySelectorAll('[data-component]'));
+	const existedComponents = Array.from(document.querySelectorAll('[data-component]'));
 
-    const components = existedComponents.map((component) => {
-        try {
-            return new allComponents[component.dataset.component]({
-                name: component.dataset.component,
-                component: component,
-            });
-        } catch (e) {
-            console.error(`Ошибка во время инициализации компонента: ${component.dataset.component}\n\n${e}`);
-        }
-    });
+	const components = existedComponents.map((component) => {
+		try {
+			return new allComponents[component.dataset.component]({
+				name: component.dataset.component,
+				component: component,
+			});
+		} catch (e) {
+			console.error(`Ошибка во время инициализации компонента: ${component.dataset.component}\n\n${e}`);
+		}
+	});
 } catch (e) {
-    console.error(e);
+	console.error(e);
 }
---
+++ src/components/form/form.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/Component';
+
+class Form extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default Form
