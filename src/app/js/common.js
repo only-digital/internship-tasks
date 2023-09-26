8cf89eedf671977ec68c4535bb53819b1d9f4aee
ShaggyBa <71100001+ShaggyBa@users.noreply.github.com>
2023-09-26 15:43:52 +0300

    refactoring spoiler component add faq component

--- src/app/js/common.js
+++ src/app/js/common.js
@@ -1,13 +1,16 @@
 import ExampleButton from "../../components/example-button/example-button";
 import Spoiler from "../../components/spoiler/spoiler";
+import FAQ from "../../components/faq/faq";
+
 /**
  * Объект со всеми компонентами, для которых будет применяться автоматическая инициализация
  * Ключ объекта - Название компонента, которое было указано во время его создания (совпадает с именем файлов)
  * Значение - JS-класс компонента (Импорт добавляется вручную)
  */
 const allComponents = {
-    'example-button': ExampleButton,
-	 'spoiler': Spoiler
+	'example-button': ExampleButton,
+	'spoiler': Spoiler,
+	'faq': FAQ
 }
 
 
@@ -15,18 +18,18 @@ const allComponents = {
  * Инициализация всех компонентов на странице
  */
 try {
-    const existedComponents = Array.from(document.querySelectorAll('[data-component]'));
+	const existedComponents = Array.from(document.querySelectorAll('[data-component]'));
 
-    const components = existedComponents.map((component) => {
-        try {
-            return new allComponents[component.dataset.component]({
-                name: component.dataset.component,
-                component: component,
-            });
-        } catch (e) {
-            console.error(`Ошибка во время инициализации компонента: ${component.dataset.component}\n\n${e}`);
-        }
-    });
+	const components = existedComponents.map((component) => {
+		try {
+			return new allComponents[component.dataset.component]({
+				name: component.dataset.component,
+				component: component,
+			});
+		} catch (e) {
+			console.error(`Ошибка во время инициализации компонента: ${component.dataset.component}\n\n${e}`);
+		}
+	});
 } catch (e) {
-    console.error(e);
+	console.error(e);
 }
--- src/components/spoiler/spoiler.js
+++ src/components/spoiler/spoiler.js
@@ -1,34 +1,30 @@
 import Component from '../../app/js/base/Component';
 
 class Spoiler extends Component {
-	spoilers;
-    constructor(element) {
+	spoilerHeader;
+	constructor(element) {
 		super(element);
 
-		this.spoilers = this.getElements('item')
+		this.spoilerHeader = this.getElement('header')
 
-		for (let spoiler of this.spoilers) {
-			const headerOfSpoiler = spoiler.querySelector('.spoiler__item__header')
-
-			headerOfSpoiler.addEventListener('click', this.onButtonClick)	
-		}	
+		this.spoilerHeader.addEventListener('click', this.onButtonClick)
 	}
 
-	 onButtonClick = (e) => {
+	onButtonClick = (e) => {
 		const target = e.target
-		const textContent = target.parentNode.nextSibling 
+		const textContent = target.parentNode.nextSibling
 
-		target.closest('.spoiler__item').classList.toggle('open')
-		this.smoothSpoiler(textContent)		
-	 }
+		target.closest('.spoiler').classList.toggle('open')
+		this.smoothSpoiler(textContent)
+	}
 
-	 smoothSpoiler = (el) => {
-		if (el.closest('.spoiler__item').classList.contains('open')) {
+	smoothSpoiler = (el) => {
+		if (el.closest('.spoiler').classList.contains('open')) {
 			el.style.height = el.scrollHeight + 'px'
 		}
-		else 
+		else
 			el.style.height = 0;
-	 }
+	}
 }
 
 export default Spoiler
