ac45d03a7284a73f1024fa348d960558d2c6bd3e
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2023-09-28 17:18:01 +0300

    added component to the main page and finalized the page

--- src/app/js/common.js
+++ src/app/js/common.js
@@ -1,4 +1,5 @@
 import ExampleButton from "../../components/example-button/example-button";
+import Accordion from '../../components/accordion/accordion';
 
 /**
  * Объект со всеми компонентами, для которых будет применяться автоматическая инициализация
@@ -7,6 +8,7 @@ import ExampleButton from "../../components/example-button/example-button";
  */
 const allComponents = {
     'example-button': ExampleButton,
+    'accordion': Accordion,
 }
 
 
