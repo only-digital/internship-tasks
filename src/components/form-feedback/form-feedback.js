11afa526c39aa7dfad492abbc12cd64da6d70f7f
Ermak Anna <ermak-anna86@yandex.ru>
2023-09-28 20:05:01 +0300

    add filed email

---
+++ src/components/form-feedback/form-feedback.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/Component';
+
+class FormFeedback extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default FormFeedback
