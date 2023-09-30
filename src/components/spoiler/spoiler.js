776d7e187bb97f8bc786cf5e2695e1ca15a92d14
Irina <irinka-akva@ya.ru>
2023-09-30 19:33:23 +0300

    Добавляет скрипт и завершает верстку спойлеров

--- src/components/spoiler/spoiler.js
+++ src/components/spoiler/spoiler.js
@@ -4,10 +4,27 @@ class Spoiler extends Component {
     constructor(element) {
         super(element);
 
-        // Your code here
+        // Находим все кнопки и присваиваем их в переменную
+
+        const buttons = this.getElements("button")
+
+        // Навешиваем обработчики события клика на кнопки
+
+        buttons.forEach(button => {
+            button.addEventListener("click", this.onButtonClick)
+        })
     }
 
-    // Your code here
+    // Обработчик события нажатия на кнопку в спойлере
+
+    onButtonClick = () => {
+        this.root.classList.toggle("open")
+        if (this.root.classList.contains("open")) {
+            this.getElement("text").style.maxHeight = this.getElement("text").scrollHeight + "px"
+        } else {
+            this.getElement("text").style.maxHeight = 0
+        }
+    }
 }
 
 export default Spoiler
