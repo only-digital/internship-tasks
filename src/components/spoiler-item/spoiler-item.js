30f226399964a1e968e264378071e4f76eb003b2
Milana <milanakhairullina@gmail.com>
2023-10-01 23:11:27 +0300

    add: add main js logic

--- src/components/spoiler-item/spoiler-item.js
+++ src/components/spoiler-item/spoiler-item.js
@@ -1,13 +1,41 @@
 import Component from '../../app/js/base/Component';
 
 class Spoileritem extends Component {
+    
+
     constructor(element) {
         super(element);
 
-        // Your code here
+        this.root.addEventListener('click', this.tabToitem);
+
     }
 
-    // Your code here
+    tabToitem = () => {
+        
+        const text = this.getElement('text');
+        const title = this.getElement('name');
+        const img = this.getElement('svg');
+
+        if (this.root.classList.contains('closed'))
+        {
+            text.style.display = 'block';
+            this.root.classList.toggle('closed');
+            
+            title.style.color = '#114C9A';
+
+            img.style.transform = 'rotate(45deg)';
+
+        }
+
+        else {
+            text.style.display = 'none';
+            this.root.classList.toggle('closed');
+
+            title.style.color = '#012B34';
+
+            img.style.transform = 'rotate(90deg)';
+        }
+    }
 }
 
 export default Spoileritem
--- src/components/spoiler/spoiler.js
+++ src/components/spoiler/spoiler.js
@@ -4,10 +4,8 @@ class Spoiler extends Component {
     constructor(element) {
         super(element);
 
-        // Your code here
     }
 
-    // Your code here
 }
 
 export default Spoiler
