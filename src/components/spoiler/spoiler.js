83530cde37b26fa8529589f2eac840ea34ba5fce
Whittun <whittun@gmail.com>
2023-10-02 17:27:37 +0300

    feat: add accordion functionality

--- src/components/spoiler/spoiler.js
+++ src/components/spoiler/spoiler.js
@@ -1,13 +1,26 @@
 import Component from '../../app/js/base/Component';
 
 class Spoiler extends Component {
+    spoiler;
+    textContent;
+
     constructor(element) {
         super(element);
 
-        // Your code here
-    }
+        this.spoiler = this.getElement('main-wrapper')
+        this.textContent = this.getElement('bottom-wrapper')
 
-    // Your code here
+        this.root.addEventListener('click', this.onSliderClick)
+    }
+    onSliderClick = () => {
+        this.root.classList.toggle('spoiler--active')
+
+        if (this.textContent.style.maxHeight) {
+            this.textContent.style.maxHeight = null;
+        } else {
+            this.textContent.style.maxHeight = this.textContent.scrollHeight + 'px';
+        }
+    }
 }
 
 export default Spoiler
