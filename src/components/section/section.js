7a512725ab0485c263a6343d4b8affeb5fcdb88c
Dmitry Litvinenko <ds-sev@ya.ru>
2023-10-02 17:22:42 +0300

    feat: add media queries for adptive design

--- src/components/section/section.js
+++ src/components/section/section.js
@@ -3,12 +3,7 @@ import Component from '../../app/js/base/Component';
 class Section extends Component {
     constructor(element) {
         super(element);
-        console.log('mnmnk,mkknj')
-
-        // Your code here
     }
-
-    // Your code here
 }
 
 export default Section
