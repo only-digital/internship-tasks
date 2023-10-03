acc4f874a199f8dde9a9f53c7b6bbc773eb57fbd
Amin Hesenov <lolotron022gmail.com>
2023-10-03 15:45:34 +0400

    reserve save

---
+++ src/components/footer/footer.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/Component';
+
+class Footer extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default Footer
