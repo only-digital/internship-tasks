0f2f90e831d9125987748001e34c666afc1630f5
Eugene Ermolaev <79959654+flab3rt@users.noreply.github.com>
2023-10-04 09:15:02 +0000

    импортировал компонент accordeon в common.js, добавил ключ/значение в allComponents

--- src/app/js/common.js
+++ src/app/js/common.js
@@ -1,4 +1,5 @@
 import ExampleButton from "../../components/example-button/example-button";
+import Accordeon from "../../components/accordeon/accordeon";
 
 /**
  * Объект со всеми компонентами, для которых будет применяться автоматическая инициализация
@@ -7,6 +8,7 @@ import ExampleButton from "../../components/example-button/example-button";
  */
 const allComponents = {
     'example-button': ExampleButton,
+    'accordeon': Accordeon
 }
 
 
