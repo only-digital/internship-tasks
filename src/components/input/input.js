414a4aff3382ad04f48a24fcb9d865a71a705601
Vadim Manushin <vadimmanushin@yandex.ru>
2023-10-05 19:57:50 +0300

    input added

---
+++ src/components/input/input.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/Component';
+
+class Input extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default Input
