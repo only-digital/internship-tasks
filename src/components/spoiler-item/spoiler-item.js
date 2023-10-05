c01b2c3dea509509b5402306a5bdd2edc5043e21
volokhinn <csgomx@mail.ru>
2023-10-05 23:04:27 +0700

    task 3 is done

--- src/components/spoiler-item/spoiler-item.js
+++ src/components/spoiler-item/spoiler-item.js
@@ -1,15 +1,14 @@
 import Component from '../../app/js/base/Component';
 
 class SpoilerItem extends Component {
-    head;
-    constructor(element) {
-        super(element);
-        this.root.addEventListener("click", this.toggleSpoiler.bind(this));
-    }
+  constructor(element) {
+    super(element);
+    this.root.addEventListener('click', this.toggleSpoiler.bind(this));
+  }
 
-    toggleSpoiler() {
-        this.root.classList.toggle('spoiler-item__active');
-    }
+  toggleSpoiler() {
+    this.root.classList.toggle('spoiler-item__active');
+  }
 }
 
 export default SpoilerItem;
