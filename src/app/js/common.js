1e6eee88615419a417f4264baaca4ecd67e9b1e5
Amin Hesenov <lolotron022gmail.com>
2023-10-05 23:53:56 +0400

    v1

--- src/app/js/common.js
+++ src/app/js/common.js
@@ -1,4 +1,3 @@
-import ExampleButton from "../../components/example-button/example-button";
 
 /**
  * Объект со всеми компонентами, для которых будет применяться автоматическая инициализация
@@ -6,7 +5,6 @@ import ExampleButton from "../../components/example-button/example-button";
  * Значение - JS-класс компонента (Импорт добавляется вручную)
  */
 const allComponents = {
-    'example-button': ExampleButton,
 }
 
 /**
