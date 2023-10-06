b9ef242ab2386f0ac9022eb7aae747e626c7ad8f
Nastya <nkartsel@gmail.com>
2023-10-06 22:57:00 +0300

    fix: accordion.js

--- src/components/accordion/accordion.js
+++ src/components/accordion/accordion.js
@@ -1,19 +1,28 @@
 import Component from '../../app/js/base/Component';
 
 class Accordion extends Component {
+    
+    text;
+    title;
+
     constructor(element) {
         super(element);
-
-        // Your code here
-        this.root.addEventListener("click", this.onClick) // Добавление обработчика события click
+        this.text = this.getElement('text');
+        this.title = this.getElement('title');
+            
+        this.title.addEventListener("click", this.onClick);
     }
 
     // Your code here
-    onClick = (event) => {                
-        if(event.target.closest(".accordion")) {   //если кликнутый эл-т явл потомком accordion
-            console.log(event.target)
-            this.root.classList.toggle("active")        //добавить active корнев элту accordion
-        }
+    onClick = () => {                
+        
+        this.root.classList.toggle("active")        //добавить active корнев элту accordion
+
+        if (this.root.classList.contains('active')) {
+            this.text.style.height = this.text.scrollHeight + 'px';
+        } else {
+            this.text.style.height = 0;
+        }    
     }
 }
 
