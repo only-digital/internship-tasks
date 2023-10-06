7961707d432b68ab3e53f1923f1cb1a78faea234
katerina-shche <asti.katerina@gmail.com>
2023-10-06 14:14:03 +0200

    set up

---
+++ src/components/vacancy/vacancy.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/component';
+
+class Vacancy extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default Vacancy
