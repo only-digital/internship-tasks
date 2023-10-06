5982c29d3860772c7ce8c12e8b8c1dc6797503a0
Artem Egorov <artegtor@gmail.com>
2023-10-06 10:38:38 +0500

    finish task-3

--- src/app/js/common.js
+++ src/app/js/common.js
@@ -1,4 +1,5 @@
 import ExampleButton from "../../components/example-button/example-button";
+import SpoilerCustom from '../../components/spoiler-custom/spoiler-custom';
 
 /**
  * Объект со всеми компонентами, для которых будет применяться автоматическая инициализация
@@ -7,6 +8,7 @@ import ExampleButton from "../../components/example-button/example-button";
  */
 const allComponents = {
     'example-button': ExampleButton,
+    'spoiler-custom': SpoilerCustom,
 }
 
 
