87d583c5740b18583ede074518a93cd815e154f5
unknown <Lihtinmann88@yandex.ru>
2023-10-06 18:27:24 +0300

    clean up a bit

--- src/components/spoiler/spoiler.js
+++ src/components/spoiler/spoiler.js
@@ -3,11 +3,14 @@ import Component from '../../app/js/base/Component';
 class Spoiler extends Component {
     captionElement;
     descriptionElement;
+    svgElement;
 
     constructor(element) {
         super(element);
 
         this.captionElement = this.getElement('caption');
+
+        this.svgElement = this.getElement('caption__svg');
 
         this.descriptionElement = this.getElement('description');
 
@@ -18,9 +21,11 @@ class Spoiler extends Component {
         if (this.descriptionElement.offsetHeight === 0) {
           this.descriptionElement.style.maxHeight = this.descriptionElement.scrollHeight + "px";
           this.descriptionElement.style.marginBottom = window.getComputedStyle (this.captionElement).marginBottom ;
+          this.svgElement.style.rotate = '45deg';
         } else{
           this.descriptionElement.style.maxHeight = "0";
           this.descriptionElement.style.marginBottom = "0px";
+          this.svgElement.style.rotate = '180deg';
         }
 
       }
