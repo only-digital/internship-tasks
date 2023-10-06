ae08e749c60d57198c4a4d6f9d527b25155857cd
dsa25 <73094994+dsa25@users.noreply.github.com>
2023-10-06 15:30:22 +0500

    layout

---
+++ src/components/vacancy-info/vacancy-info.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/component';
+
+class VacancyInfo extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default VacancyInfo
