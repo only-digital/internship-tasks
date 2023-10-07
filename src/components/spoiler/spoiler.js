0a1cba41e88ae34c52a88a3f8b639e18ef99f6fa
Whittun <whittun@gmail.com>
2023-10-07 19:12:24 +0300

    refacror: make spoiler animation on css

--- src/components/spoiler/spoiler.js
+++ src/components/spoiler/spoiler.js
@@ -2,24 +2,16 @@ import Component from '../../app/js/base/Component';
 
 class Spoiler extends Component {
     spoiler;
-    textContent;
 
     constructor(element) {
         super(element);
 
         this.spoiler = this.getElement('main-wrapper')
-        this.textContent = this.getElement('bottom-wrapper')
 
         this.root.addEventListener('click', this.onSpoilerClick)
     }
     onSpoilerClick = () => {
         this.root.classList.toggle('spoiler--opened')
-
-        if (this.textContent.style.maxHeight) {
-            this.textContent.style.maxHeight = null;
-        } else {
-            this.textContent.style.maxHeight = this.textContent.scrollHeight + 'px';
-        }
     }
 }
 
