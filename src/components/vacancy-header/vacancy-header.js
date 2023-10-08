f33b7282b60acf2fe0cffb874c45fe633ba5e755
volokhinn <csgomx@mail.ru>
2023-10-08 13:22:28 +0700

    Added all components

---
+++ src/components/vacancy-header/vacancy-header.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/component';
+
+class VacancyHeader extends Component {
+  textElement;
+  count;
+
+  constructor(element) {
+    super(element);
+  }
+  //
+}
+
+export default VacancyHeader;
