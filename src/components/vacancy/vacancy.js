92aecaea5fe907cdd5126a3fd1d2576c2417fa1d
katerina-shche <asti.katerina@gmail.com>
2023-10-08 14:33:54 +0200

    basic styles and js set up

--- src/components/vacancy/vacancy.js
+++ src/components/vacancy/vacancy.js
@@ -1,8 +1,21 @@
 import Component from '../../app/js/base/component';
 
 class Vacancy extends Component {
+    views;
+    responses;
+
     constructor(element) {
         super(element);
+        //grabing DOM elements
+        this.date = this.getElement('date');
+        this.views = this.getElement('views');
+        this.responses = this.getElement('responses');
+        console.log(new Date)
+        // setting inner content
+        this.date.innerText = new Intl.DateTimeFormat('ru', { year: 'numeric', month: 'long', day: 'numeric' }).format(new Date());
+        this.views.innerText = 'Просмотров: 123';
+        this.responses.innerText = 'Откликов: 12';
+       
 
         // Your code here
     }
