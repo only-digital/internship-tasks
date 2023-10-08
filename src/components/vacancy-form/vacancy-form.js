f08e1bfa626648446566cdcb250b7b3a691d4821
volokhinn <csgomx@mail.ru>
2023-10-08 14:05:56 +0700

    Added script for views and responses

---
+++ src/components/vacancy-form/vacancy-form.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/component';
+
+class VacancyForm extends Component {
+  textElement;
+  count;
+
+  constructor(element) {
+    super(element);
+  }
+  //
+}
+
+export default VacancyForm;
--- src/components/vacancy-header/vacancy-header.js
+++ src/components/vacancy-header/vacancy-header.js
@@ -1,13 +1,28 @@
 import Component from '../../app/js/base/component';
 
 class VacancyHeader extends Component {
-  textElement;
-  count;
+  URL;
+  stats;
+  views;
+  responses;
 
   constructor(element) {
     super(element);
+    this.URL = '/stats';
+    this.stats = this.getElement('stats');
+    this.views = this.getElement('views');
+    this.responses = this.getElement('responses');
+    document.addEventListener('DOMContentLoaded', this.getStats);
   }
-  //
+
+  getStats = () => {
+    fetch(this.URL)
+      .then((response) => response.json())
+      .then((stats) => {
+        this.views.textContent = stats.views;
+        this.responses.textContent = stats.responses;
+      });
+  };
 }
 
 export default VacancyHeader;
