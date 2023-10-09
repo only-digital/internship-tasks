b755a42d3ac69a3571e0588f6eae619e411df58a
Amin Hesenov <lolotron022gmail.com>
2023-10-09 17:43:37 +0400

    reserve copy

---
+++ src/components/feedback/form/conf-checkbox-ct/conf-checkbox-ct.js
@@ -0,0 +1,13 @@
+import Component from '../../../../app/js/base/Component';
+
+class textareaCt extends Component {
+    input;
+    name;
+
+    constructor(element) {
+        super(element);
+        this.textareaCondintion = false;
+    }
+}
+
+export default textareaCt
