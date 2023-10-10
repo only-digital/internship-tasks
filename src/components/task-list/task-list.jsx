d72de6ab2116a4f6fe18e30efd8d062bb9ffa7b9
Dmitry Shevtsov <dshevtsov20@gmail.com>
2023-10-10 14:40:11 +0300

    change task-list

--- src/components/task-list/task-list.jsx
+++ src/components/task-list/task-list.jsx
@@ -1,26 +1,30 @@
 import Index from "@/pages";
 import TaskItem from "../task-item/task-item";
 import styled from "./task-list.module.scss";
+import { useEffect } from "react";
 
-const TaskList = ({ tasks, setTasks }) => {
+const TaskList = ({ tasks, allTasks, setTasks }) => {
   return (
     <div className={styled.TaskList}>
-      {tasks.map((task, index) => (
-        <TaskItem
-          key={index}
-          item={task}
-          onDelete={() => {
-            const newTasks = tasks.filter((item, i) => i !== index);
-            setTasks(newTasks);
-          }}
-          toggleComplete={() => {
-            const newTasks = tasks.map((item, i) =>
-              i !== index ? item : { ...item, isCompleted: !item.isCompleted }
-            );
-            setTasks(newTasks);
-          }}
-        />
-      ))}
+      {tasks &&
+        tasks.map((task, index) => (
+          <TaskItem
+            key={index}
+            item={task}
+            onDelete={() => {
+              const newTasks = allTasks.filter((item) => item.id !== task.id);
+              setTasks(newTasks);
+            }}
+            toggleComplete={() => {
+              const newTasks = allTasks.map((item) =>
+                item.id === task.id
+                  ? { ...item, isCompleted: !item.isCompleted }
+                  : item
+              );
+              setTasks(newTasks);
+            }}
+          />
+        ))}
     </div>
   );
 };
