951af3b75b21093dd0e556fa26348bc368b66259
Vadim Manushin <vadimmanushin@yandex.ru>
2023-10-10 22:56:59 +0300

    logic of handling email input correct removed into form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,19 +1,148 @@
 import Component from '../../app/js/base/Component';
 
 class Form extends Component {
+
     serverError;
+    policyError;
+    policyInput;
+    loader;
+    submitButton;
+    errors;
+    inputEmailRoot;
+    inputEmail;
+    errorEmail;
+    tipEmail;
 
     constructor(element) {
         super(element);
 
         this.root.addEventListener('submit',this.handleFormSubmit);
-        this.serverError = this.getElement('server-error')
+        this.serverError = this.getElement('server-error');
+        this.policyError = this.getElement('policy-error');
+        this.loader = this.root.querySelector('.loader');
+        this.submitButton = this.root.querySelector('.button');
+        this.inputEmailRoot = this.root.querySelector('.input-email');
+        this.inputEmail = this.inputEmailRoot.querySelector('.input-email__input');
+        this.errorEmail = this.inputEmailRoot.querySelector('.input-email__error');
+        this.tipEmail = this.inputEmailRoot.querySelector('.input-email__svg');
+        this.policyInput = this.root.querySelector('.checkbox__input');
+        this.policyInput.addEventListener('change',this.handlePolicyInput);
+        this.inputEmail.addEventListener('input',this.handleEmailInput);
+        this.inputEmail.addEventListener('focus',this.handleEmailFocus);
+        this.errors = new Map([
+                                ['email',  false],
+                                ['textarea', false],
+                                ['policy', false]
+                            ]);
+        this.disableSubmitButton();
+    }
+
+    handleEmailInput = (event) => {
+        const emailRegExp = /^([a-zA-Z\-0-9_]+|([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)+)|(".+"))@(([a-zA-Z\-0-9]+\.)+[a-zA-Z\-0-9]{2,})$/;
+        if (!emailRegExp.test(event.target.value)) {
+            this.showEmailError('Введите email');
+            this.hideEmailTip();
+            this.errors.set('email',false);
+        } else if (event.target.value.length > 255) {
+            this.showEmailError('Введите значение менее 255 символов');
+            this.hideEmailTip();
+            this.errors.set('email',false);
+        } else {
+            this.hideEmailError();
+            this.showEmailTip();
+            this.errors.set('email',true);
+        }
+        this.checkErrors();
+    }
+
+    handlePolicyInput = (event) => {
+        if(event.target.checked) {
+            this.policyError.classList.add('form_invisible-elem');
+            this.errors.set('policy',true);
+        } else {
+            this.errors.set('policy',false);
+            this.policyError.classList.remove('form_invisible-elem');
+        }
+        this.checkErrors();
+    }
+
+    checkErrors = () => {
+        let formIsValid = false;
+        for (let value of this.errors.values()) {
+            if (value) {
+                formIsValid = true
+            } else formIsValid = false;
+        }
+        if (formIsValid) {
+            this.enableSubmitButton();
+        } else this.disableSubmitButton();
+    }
+
+    enableSubmitButton = () => {
+        this.submitButton.classList.remove('button_disabled');
+    }
+
+    showEmailTip = () => {
+        this.tipEmail.classList.remove('input-email_invisible');
+    }
+
+    hideEmailTip = () => {
+        if (!this.tipEmail.classList.contains('input-email_invisible')) {
+            this.tipEmail.classList.add('input-email_invisible');
+        }
+    }
+
+    showEmailError = (error) =>{
+        this.errorEmail.classList.remove('input-email_invisible');
+        this.inputEmail.classList.add('input-email__input_error');
+        const prevError = this.errorEmail.innerText;
+        if (!prevError.includes(error)) {
+            this.errorEmail.innerText = prevError + ' ' + error;
+        }
+    }
+
+    hideEmailError = () => {
+        this.errorEmail.innerText = '';
+        this.errorEmail.classList.add('input-email_invisible');
+        this.inputEmail.classList.remove('input-email__input_error');
+    }
+
+    handleEmailFocus = () => {
+        this.hideEmailError();
+    }
+
+    hideEmailError = () => {
+        this.errorEmail.innerText = '';
+        this.errorEmail.classList.add('input-email_invisible');
+        this.inputEmail.classList.remove('input-email__input_error');
+    }
+
+    showLoader = () => {
+        this.loader.classList.remove('loader_disabled')
+    }
+
+    hideLoader = () => {
+        this.loader.classList.add('loader_disabled')
+    }
+
+    disableSubmitButton = () => {
+        if (!this.submitButton.classList.contains('button_disabled')) {
+            this.submitButton.classList.add('button_disabled');
+        }
     }
 
     handleFormSubmit = (event) => {
         event.preventDefault();
         const formData = new FormData(this.root);
         console.log(formData);
+        if (!formData.get('policy')) {
+            this.setPolicyError();
+        } else {
+            this.submitData({
+                email: formData.get('email'),
+                policy: true,
+            })
+        }
     }
 
     setError = (errorText) => {
@@ -24,6 +153,14 @@ class Form extends Component {
     clearError = () => {
         this.serverError.textContent = '';
         this.serverError.classList.add('form_invisible-elem');
+    }
+
+    setPolicyError = () => {
+        this.policyError.classList.remove('form_invisible-elem');
+    }
+
+    clearPolicyError = () => {
+        this.policyError.classList.add('form_invisible-elem');
     }
 
     submitData = async (data) => {
@@ -39,9 +176,9 @@ class Form extends Component {
                 body:JSON.stringify(data)
             });
             this.hideLoader();
-            if (response.ok) {
-                this.setSuccess();
-            } else this.setError(response.statusText);
+            if (!response.ok) {
+                this.setError(response.statusText);
+            }
         } catch (error) {
             console.error(error);
         }
--- src/components/input-email/input-email.js
+++ src/components/input-email/input-email.js
@@ -1,67 +1,8 @@
 import Component from '../../app/js/base/Component';
 
 class InputEmail extends Component {
-    input;
-    error;
-    tip;
     constructor(element) {
         super(element);
-        this.root = element.component;
-        this.input = this.getElement('input');
-        this.error = this.getElement('error');
-        this.tip = this.getElement('svg');
-        this.input.addEventListener('blur',this.handleEmailInput);
-        this.input.addEventListener('focus',this.handleEmailFocus);
-    }
-
-    showError = (error) =>{
-        this.error.classList.remove('input-email_invisible');
-        this.input.classList.add('input-email__input_error');
-        const prevError = this.error.innerText;
-        this.error.innerText = prevError+ ' '+error;
-    }
-
-    showTip = () => {
-        this.tip.classList.remove('input-email_invisible');
-    }
-
-    hideTip = () => {
-        if (this.tip.classList.contains('input-email_invisible')) {
-            this.tip.classList.add('input-email_invisible');
-        }
-    }
-
-    setConfirmAttribure = (isConfirmed) => {
-        if (isConfirmed) {
-            this.root.setAttribute('data-valid','true');
-        } else this.root.removeAttribute('data-valid');
-    }
-
-    hideError = () => {
-        this.error.innerText = '';
-        this.error.classList.add('input-email_invisible');
-        this.input.classList.remove('input-email__input_error');
-    }
-
-    handleEmailInput = (event) => {
-        const emailRegExp = /^([a-zA-Z\-0-9_]+|([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)+)|(".+"))@(([a-zA-Z\-0-9]+\.)+[a-zA-Z\-0-9]{2,})$/;
-        if (!emailRegExp.test(event.target.value)) {
-            this.showError('Введите email');
-            this.hideTip();
-            this.setConfirmAttribure(false);
-        } else if (event.target.value.length > 255) {
-            this.showError('Введите значение менее 255 символов');
-            this.hideTip();
-            this.setConfirmAttribure(false);
-        } else {
-            this.hideError();
-            this.showTip();
-            this.setConfirmAttribure(true);
-        }
-    }
-
-    handleEmailFocus = () => {
-        this.hideError();
     }
 }
 
