282563e61c96c6b7cf31c64643675c9013fcfeff
TimmyGray <zuuubada@inbox.ru>
2023-10-10 22:08:26 +0300

    init. created components and styles files

---
+++ src/components/only-parag/only-parag.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/Component';
+
+class OnlyParag extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default OnlyParag
---
+++ src/components/only-vacancy-wrapper/only-vacancy-wrapper.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/Component';
+
+class OnlyVacancyWrapper extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default OnlyVacancyWrapper
