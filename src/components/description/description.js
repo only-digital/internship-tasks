21b601693c7250fffefb6c87ae445ba444b3b561
Whittun <whittun@gmail.com>
2023-10-10 18:00:42 +0300

    feat: create the required components

---
+++ src/components/description/description.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/Component';
+
+class Description extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default Description
