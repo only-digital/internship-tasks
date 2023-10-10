875e1cf12564ce74caa4bb92d41d27ff988bc269
Vadim Manushin <vadimmanushin@yandex.ru>
2023-10-10 20:36:44 +0300

    added submit data method into form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,19 +1,50 @@
 import Component from '../../app/js/base/Component';
 
 class Form extends Component {
-    form;
+    serverError;
 
     constructor(element) {
         super(element);
-        console.log(this.root)
 
         this.root.addEventListener('submit',this.handleFormSubmit);
+        this.serverError = this.getElement('server-error')
     }
 
     handleFormSubmit = (event) => {
         event.preventDefault();
         const formData = new FormData(this.root);
         console.log(formData);
+    }
+
+    setError = (errorText) => {
+        this.serverError.classList.remove('form_invisible-elem');
+        this.serverError.textContent = errorText;
+    }
+
+    clearError = () => {
+        this.serverError.textContent = '';
+        this.serverError.classList.add('form_invisible-elem');
+    }
+
+    submitData = async (data) => {
+        this.clearError();
+        const url = 'http://localhost:3000/form';
+        try {
+            this.showLoader();
+            const response = await fetch(url,{
+                method:'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body:JSON.stringify(data)
+            });
+            this.hideLoader();
+            if (response.ok) {
+                this.setSuccess();
+            } else this.setError(response.statusText);
+        } catch (error) {
+            console.error(error);
+        }
     }
 
 }
