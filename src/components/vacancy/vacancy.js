55193a6f75b433a32d09875ad024e44899a76e35
katerina-shche <asti.katerina@gmail.com>
2023-10-11 10:12:37 +0200

    fetch (views and responses)

--- src/components/vacancy/vacancy.js
+++ src/components/vacancy/vacancy.js
@@ -3,6 +3,7 @@ import Component from '../../app/js/base/component';
 class Vacancy extends Component {
     views;
     responses;
+    data;
 
     constructor(element) {
         super(element);
@@ -13,9 +14,18 @@ class Vacancy extends Component {
         console.log(new Date)
         // setting inner content
         this.date.innerText = new Intl.DateTimeFormat('ru', { year: 'numeric', month: 'long', day: 'numeric' }).format(new Date());
-        this.views.innerText = 'Просмотров: 123';
-        this.responses.innerText = 'Откликов: 12';
+        this.views.innerText = 'Просмотров:';
+        this.responses.innerText = 'Откликов:';
        
+        fetch(`/stats`)
+            .then(response => this.data = response.json())
+            .then(data => {
+                console.log(data.responses, data.views)
+                this.views.innerText += ' ' + data.views;
+                this.responses.innerText += ' ' + data.responses;
+                }
+            )
+            .catch(error => console.log(error))
 
         // Your code here
     }
