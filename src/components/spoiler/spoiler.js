859155ac355d2dfcbb9bfdafa1425c040b8d171f
Nebesnaya94 <kuroi.alice.13@gmail.com>
2023-10-11 17:29:20 +0300

    fix required changes in task-3

--- src/components/spoiler/spoiler.js
+++ src/components/spoiler/spoiler.js
@@ -4,11 +4,8 @@ class Spoiler extends Component {
   constructor(element) {
     super(element);
 
-    // Your code here
     this.root.addEventListener("click", this.onSpoilerExpose);
   }
-
-  // Your code here
 
   onSpoilerExpose = () => {
     this.root.classList.toggle("active");
