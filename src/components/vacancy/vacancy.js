1e64cffb8356a14481737137d15f8cb4746ca5c7
katerina-shche <asti.katerina@gmail.com>
2023-10-11 10:48:37 +0200

    button

--- src/components/vacancy/vacancy.js
+++ src/components/vacancy/vacancy.js
@@ -4,6 +4,7 @@ class Vacancy extends Component {
     views;
     responses;
     data;
+    form;
 
     constructor(element) {
         super(element);
@@ -11,12 +12,14 @@ class Vacancy extends Component {
         this.date = this.getElement('date');
         this.views = this.getElement('views');
         this.responses = this.getElement('responses');
+        this.form = this.getElement('form');
         console.log(new Date)
-        // setting inner content
+        // setting header inner content
         this.date.innerText = new Intl.DateTimeFormat('ru', { year: 'numeric', month: 'long', day: 'numeric' }).format(new Date());
         this.views.innerText = 'Просмотров:';
         this.responses.innerText = 'Откликов:';
        
+        //fetch views and responses
         fetch(`/stats`)
             .then(response => this.data = response.json())
             .then(data => {
@@ -27,10 +30,14 @@ class Vacancy extends Component {
             )
             .catch(error => console.log(error))
 
-        // Your code here
+        // form input handler
+        this.form.addEventListener('submit', (e) => this.onSubmit(e))
     }
 
-    // Your code here
+    // handle Submit
+    onSubmit = (e) => {
+        e.preventDefault();
+    }
 }
 
 export default Vacancy
