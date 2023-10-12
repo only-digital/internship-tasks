3bb2df7f44efbf38679d8f28bd18b7eff6cd22ed
katerina-shche <asti.katerina@gmail.com>
2023-10-12 14:15:19 +0200

    correct data fetching to '/form'

--- src/components/vacancy/vacancy.js
+++ src/components/vacancy/vacancy.js
@@ -5,6 +5,10 @@ class Vacancy extends Component {
     responses;
     data;
     form;
+    formData;
+    formDataAsObject;
+    email;
+    confirm;
 
     constructor(element) {
         super(element);
@@ -12,12 +16,13 @@ class Vacancy extends Component {
         this.date = this.getElement('date');
         this.views = this.getElement('views');
         this.responses = this.getElement('responses');
-        this.form = this.getElement('form');
-        console.log(new Date)
+        this.email = this.getElement('email');
+        this.confirm = this.getElement('confirm');
         // setting header inner content
         this.date.innerText = new Intl.DateTimeFormat('ru', { year: 'numeric', month: 'long', day: 'numeric' }).format(new Date());
         this.views.innerText = 'Просмотров:';
         this.responses.innerText = 'Откликов:';
+        
        
         //fetch views and responses
         fetch(`/stats`)
@@ -31,12 +36,44 @@ class Vacancy extends Component {
             .catch(error => console.log(error))
 
         // form input handler
-        this.form.addEventListener('submit', (e) => this.onSubmit(e))
+        
+        this.root.addEventListener('submit', (e) => this.onSubmit(e))
     }
 
     // handle Submit
     onSubmit = (e) => {
+        console.log(e.target);
+        console.log(email.value);
+        console.log(confirm.value);
         e.preventDefault();
+        // Collect form data
+        this.form = this.getElement('form');
+        //console.log(this.form)
+        this.formData = new FormData(form);
+        this.formData.set('confirm', true)
+        console.log(this.formData.get('email'), this.formData.get('confirm') )
+        //console.log('myformData:', this.formData.entries);
+        // Perform the POST request using fetch()
+        this.formDataAsObject = {};
+        for (const [key, value] of this.formData.entries()) {
+        this.formDataAsObject[key] = value;
+        }
+
+        fetch(`/form`, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(this.formDataAsObject)
+            })
+        .then(response => {
+      // Handle the response here
+        console.log(response.json())
+        })
+        .catch(error => {
+      // Handle errors here
+        console.log(error)
+        });
     }
 }
 
