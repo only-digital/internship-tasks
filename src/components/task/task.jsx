9133eefed281af5138c2e8c4671985297cfd47c1
volokhinn <csgomx@mail.ru>
2023-10-12 21:22:11 +0700

    Added displaying tasks from a file, task deletion

--- src/components/task/task.jsx
+++ src/components/task/task.jsx
@@ -1,21 +1,28 @@
-import styles from './task.module.scss'
-import Image from 'next/image'
+import styles from './task.module.scss';
+import Image from 'next/image';
+import { useState } from 'react';
 
-import { useState } from 'react'
+const Task = ({ title, text, onDelete }) => {
+  const [isCompleted, setIsCompleted] = useState(false);
 
-const Task = () => {
-    const [isCompleted, setIsCompleted] = useState(false);
+  const handleDeleteTask = (event) => {
+    event.stopPropagation();
+    onDelete();
+  };
+
   return (
     <div className={styles.task} onClick={() => setIsCompleted(!isCompleted)}>
-        <div className={styles.task__header}>
-            <h3 className={`styles.task__title ${isCompleted ? styles.task__title_completed : ''}`}>Task</h3>
-            <Image src={require('../../../public/assets/icons/close.svg')} alt="Удалить" />
-        </div>
-        <div className={styles.task__text}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi eius nemo odit velit quos inventore dolorem est reprehenderit quibusdam pariatur aperiam porro non ad distinctio, ut veritatis atque repellendus rem?
-        </div>
+      <div className={styles.task__header}>
+        <h3 className={`${styles.task__title} ${isCompleted ? styles.task__title_completed : ''}`}>
+          {title}
+        </h3>
+        <button className={styles.task__delete} onClick={handleDeleteTask}>
+          <Image src={require('../../../public/assets/icons/close.svg')} alt="Удалить" />
+        </button>
+      </div>
+      <div className={styles.task__text}>{text}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,51 @@
-import styles from '../styles/index.module.scss'
-import {getIndexPage} from "../../lib/api";
-import LinkComponent from '@/components/link/link'
+import styles from '../styles/index.module.scss';
+import { getIndexPage } from '../../lib/api';
+import LinkComponent from '@/components/link/link';
 import Header from '@/components/header/header';
 import Task from '@/components/task/task';
+import tasksData from '../../data/index.json';
+import { useState } from 'react';
 
-function Index(props) {
+function Index() {
+  const [tasks, setTasks] = useState(tasksData.tasks);
 
-    return (
-        
-        <>
-        <Header />
-        <div className={styles.layout}>
-            <div className={styles.layout__links}>
-                <LinkComponent href="/">Список задач</LinkComponent>
-            </div>
-            <div className={styles.layout__tasks}>
-            <main className={styles.main}>
-                <h1 className={styles.main__title}>Список задач</h1>
-                <Task />
-            </main>
-            </div>
+  const handleDeleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <>
+      <Header />
+      <div className={styles.layout}>
+        <div className={styles.layout__links}>
+          <LinkComponent href="/">Список задач</LinkComponent>
         </div>
-        </>
-    )
+        <div className={styles.layout__tasks}>
+          <main className={styles.main}>
+            <h1 className={styles.main__title}>{tasksData.title}</h1>
+            {tasks.map((task, index) => (
+              <Task
+                key={index}
+                title={task.title}
+                text={task.text}
+                onDelete={() => handleDeleteTask(index)}
+              />
+            ))}
+          </main>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export const getStaticProps = async () => {
-    const indexPage = await getIndexPage();
+  const indexPage = await getIndexPage();
 
-    return {
-        props: indexPage,
-        revalidate: 1
-    };
+  return {
+    props: indexPage,
+    revalidate: 1,
+  };
 };
 
 export default Index;
