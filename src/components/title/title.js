996121545a64e7e41f39fcd612397f64a1c9764f
Whittun <whittun@gmail.com>
2023-10-12 20:10:42 +0300

    feat: add a loader

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -3,18 +3,23 @@ import Component from '../../app/js/base/Component';
 class Title extends Component {
     views;
     responses;
+    vacancyData;
 
     constructor(element) {
         super(element);
 
         this.views = this.getElement('views-data')
         this.responses = this.getElement('responses-data')
+        this.vacancyData = this.getElement('vacancy-data')
         document.addEventListener('DOMContentLoaded', this.getData)
     }
 
     getData = async () => {
         const response = await fetch('/stats')
         const data = await response.json()
+
+        this.vacancyData.classList.remove('title__vacancy-data--loading')
+
         this.responses.textContent =  data.responses
         this.views.textContent = data.views
     }
