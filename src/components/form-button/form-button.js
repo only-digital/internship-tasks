8d61f6ca250d2a9611e0c13eefd752dbf9ee3c8d
Nebesnaya94 <kuroi.alice.13@gmail.com>
2023-10-14 02:40:15 +0300

    create feedback layout

---
+++ src/components/form-button/form-button.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/Component';
+
+class FormButton extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default FormButton
