76122f442afd46786c42fe315de3329f48aa653c
Milana <milanakhairullina@gmail.com>
2023-10-14 15:58:25 +0300

    add: add msg-input focus and dynamic height

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -7,18 +7,40 @@ class Form extends Component {
         this.email = this.getElement('email');
         this.emailName = this.getElement('email-name');
 
+        this.msg = this.getElement('msg');
+        this.msgName = this.getElement('msg-name');
+
         this.email.addEventListener("input", this.emailFocus)
-        //this.email.addEventListener("keyup", this.emailFocus)
+        this.msg.addEventListener("input", this.msgFocus)
     }
 
     emailFocus = () => {
         if (this.email.value!='') {
-            this.email.classList.add('active');
+            this.email.classList.add('activeEmail');
             this.emailName.classList.remove('hidden');
         }
         else {
-            this.email.classList.remove('active');
+            this.email.classList.remove('activeEmail');
             this.emailName.classList.add('hidden')
+        }
+    }
+
+    msgCalculateHeight = () => {
+        this.msg.style.height = "1px";
+        this.msg.style.height = (25+this.msg.scrollHeight)+"px";
+    }
+
+    msgFocus = () => {
+
+        this.msgCalculateHeight();
+
+        if (this.msg.value!='') {
+            this.msg.classList.add('activeMsg');
+            this.msgName.classList.remove('hidden');
+        }
+        else {
+            this.msg.classList.remove('activeMsg');
+            this.msgName.classList.add('hidden')
         }
     }
 }
