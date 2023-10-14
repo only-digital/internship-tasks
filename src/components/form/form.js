677f7a16ebdcd04335b83303c3da5d8596762d63
Milana <milanakhairullina@gmail.com>
2023-10-14 21:30:35 +0300

    add: add checkbox focus styles

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -10,11 +10,20 @@ class Form extends Component {
         this.msg = this.getElement('msg');
         this.msgName = this.getElement('msg-name');
 
+        this.checkBox = this.getElement('checkbox-input');
+        this.yesCheckBox = this.getElement("checkbox-yes");
+
         this.email.addEventListener("input", this.emailFocus)
         this.msg.addEventListener("input", this.msgFocus)
         this.msg.addEventListener("input", this.msgCalculateHeight)
+        this.checkBox.addEventListener("change", this.checkBoxChange)
     }
 
+    checkBoxChange = () => {
+        this.yesCheckBox.classList.toggle("hidden");
+        this.checkBox.classList.toggle("agree");
+
+    }
     emailFocus = () => {
         if (this.email.value!='') {
             this.email.classList.add('activeEmail');
