6eaf0a115fd10961ab606644db031585307fbea6
Lev Seregin <sereginlev1437@gmail.com>
2023-10-14 13:30:35 +0300

    added form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -7,11 +7,12 @@ class Form extends Component {
 		this.form = element.component;
 		this.emailLabel = this.getElement('email-label');
 		this.email = this.getElement('email');
+		this.emailCheck = this.getElement('email-check');
 		this.textLabel = this.getElement('text-label');
 		this.text = this.getElement('text');
 
 		document.addEventListener('click', this.onClick);
-		this.emailValidation();
+		this.email.addEventListener('keydown', this.emailValidation);
     }
 
     onClick = (e) => {
@@ -26,9 +27,11 @@ class Form extends Component {
 
 	emailValidation = () => {
 		const REG_EXP = /^([a-zA-Z\-0-9_]+|([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)+)|(".+"))@(([a-zA-Z\-0-9]+\.)+[a-zA-Z\-0-9]{2,})$/;
-
+	
 		if (this.email.value.length <= 255 && REG_EXP.test(this.email.value)) {
-			console.log(1)
+			this.emailCheck.style.display = 'block';
+			this.email.style.backgroundColor = '#F4F4F4';
+			this.email.style.border = 'none';
 		}
 	}
 }
