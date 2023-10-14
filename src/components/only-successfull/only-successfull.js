2349ff306f953d3f4f08de88c970a8d248f1ce44
TimmyGray <zuuubada@inbox.ru>
2023-10-14 13:28:11 +0300

    create component successfull

---
+++ src/components/only-successfull/only-successfull.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/component';
+
+class OnlySuccessfull extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default OnlySuccessfull
