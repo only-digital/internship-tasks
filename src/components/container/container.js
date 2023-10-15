da022624f7b597a66b6bb21dd5e79161098ddab7
Dmitriy Zakharov <dzaharov794@gmail.com>
2023-10-15 23:33:13 +0300

    add conditions component

---
+++ src/components/container/container.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/component';
+
+class Container extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default Container
