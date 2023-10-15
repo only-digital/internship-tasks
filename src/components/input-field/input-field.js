eab381ed2303565e9607492dc1fbbfa6673e90c2
dsa25 <73094994+dsa25@users.noreply.github.com>
2023-10-16 00:47:41 +0500

    input field

---
+++ src/components/input-field/input-field.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/component';
+
+class InputField extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default InputField
