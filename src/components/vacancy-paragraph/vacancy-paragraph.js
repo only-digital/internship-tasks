61a353707155bbdf2c2dabc7bca66da9029fc843
Andrey Dunaev <dunayev@yandex.ru>
2023-10-16 15:32:48 +0700

    feat создать миксин для параграфа вакансии

---
+++ src/components/vacancy-paragraph/vacancy-paragraph.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/Component';
+
+class VacancyParagraph extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default VacancyParagraph
