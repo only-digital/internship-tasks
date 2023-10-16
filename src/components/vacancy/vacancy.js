15091ec149d7576a58bb2dabffd79345e417d3af
Andrey Dunaev <dunayev@yandex.ru>
2023-10-16 21:50:38 +0700

    feat создать компонент вакансии с fetch запросом данных о просмотрах и откликах

---
+++ src/components/vacancy/vacancy.js
@@ -0,0 +1,28 @@
+import Component from '../../app/js/base/Component';
+
+class Vacancy extends Component {
+  constructor(element) {
+    super(element);
+    this.qtyViews = this.getElement('qty-views');
+    this.qtyResponses = this.getElement('qty-responses');
+    this.loader = this.getElement('loader');
+    fetch('/stats', { method: 'GET' })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return Promise.reject(`Ошибка ${res.status}`);
+        }
+      })
+      .then((data) => {
+        this.qtyResponses.textContent = `Откликов : ${data.responses}`;
+        this.qtyViews.textContent = `Просмотров : ${data.views}`;
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {
+        this.loader.classList.remove('vacancy__loader-visible');
+      });
+  }
+}
+
+export default Vacancy;
