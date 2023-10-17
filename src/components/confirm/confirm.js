338aeb6e3c95771dbb87cec88b6940186311aa50
katerina-shche <asti.katerina@gmail.com>
2023-10-17 12:35:03 +0200

    confirm

---
+++ src/components/confirm/confirm.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/component';
+
+class Confirm extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default Confirm
