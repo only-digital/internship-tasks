9ca034aae574c7aac2c7a0db8beda72bd56581e4
Alexei K <alexdevelops@outlook.com>
2023-10-17 13:57:16 +0300

    Create textarea component

---
+++ src/components/textarea/textarea.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/component';
+
+class Textarea extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default Textarea
