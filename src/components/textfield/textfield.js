1ba88073a79adea9880e1d29bece5be7fc8161bd
katerina-shche <asti.katerina@gmail.com>
2023-10-18 22:07:13 +0200

    textfield in progress

--- src/components/textfield/textfield.js
+++ src/components/textfield/textfield.js
@@ -1,13 +1,28 @@
 import Component from '../../app/js/base/component';
 
 class Textfield extends Component {
+    textfield;
+    input;
+
     constructor(element) {
         super(element);
-
+        // how to select a whole component itself??
+        this.textfield = this.getElement('');
+        console.log('this is' + this.textfield);
+        this.input = this.getElement('input');
+        console.log(this.input, this.input.value);
         // Your code here
+        this.input.addEventListener('input', this.handleInput)
     }
 
-    // Your code here
+    handleInput = () => {
+        console.log('inputing!!!')
+        console.log("Textarea value changed: " + this.input.value);
+        if (this.input.value.length > 10) {
+            this.classList.add('filled');
+            console.log('filled');
+        }
+    }    // Your code here
 }
 
 export default Textfield
