a2ccfcfe4148e2489cc3f0956427cfae1684ca85
Your Name <iraqiProfessionalDesigner@gmail.com>
2023-10-19 09:49:51 +0700

    final

---
+++ src/components/spoiler/spoiler.js
@@ -0,0 +1,34 @@
+import Component from '../../app/js/base/component';
+
+class Spoiler extends Component {
+    captionElement;
+    descriptionElement;
+    svgElement;
+
+    constructor(element) {
+        super(element);
+
+        this.captionElement = this.getElement('caption');
+
+        this.svgElement = this.getElement('caption__svg');
+
+        this.descriptionElement = this.getElement('description');
+
+        this.root.addEventListener('click', this.onSpoilerClick);
+    }
+
+    onSpoilerClick = () => {
+        if (this.descriptionElement.offsetHeight === 0) {
+          this.descriptionElement.style.maxHeight = this.descriptionElement.scrollHeight + "px";
+          this.descriptionElement.style.marginBottom = window.getComputedStyle (this.captionElement).marginBottom ;
+          this.svgElement.style.rotate = '45deg';
+        } else{
+          this.descriptionElement.style.maxHeight = "0";
+          this.descriptionElement.style.marginBottom = "0px";
+          this.svgElement.style.rotate = '180deg';
+        }
+
+      }
+}
+
+export default Spoiler
