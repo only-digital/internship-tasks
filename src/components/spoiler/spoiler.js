a3c9ee92a63b44b3a160aa34b8adce6669e39730
EkaterinaMash <ekaterina.mashinskayaa@gmail.com>
2023-10-21 20:56:26 +0300

    feat: add animation

--- src/components/spoiler/spoiler.js
+++ src/components/spoiler/spoiler.js
@@ -2,19 +2,32 @@ import Component from '../../app/js/base/Component';
 
 class Spoiler extends Component {
     spoilerContainer;
-    toggleButton;
+    paragraph;
+    spoilerContent;
+    spoilerShown;
 
     constructor(element) {
         super(element);
 
         this.spoilerContainer = this.getElement('container');
-        this.toggleButton = this.getElement('toggle-button');
+        this.paragraph = this.getElement('paragraph');
+        this.spoilerContent = this.getElement('content');
 
-        this.toggleButton.onclick = this.onButtonClick;
+        this.spoilerShown = false;
+
+        this.spoilerContainer.onclick = this.onButtonClick;
     }
 
     onButtonClick = () => {
-        this.spoilerContainer.classList.toggle('show-spoiler');
+        this.spoilerShown = !this.spoilerShown;
+
+        if (this.spoilerShown) {
+            this.spoilerContainer.classList.add('show-spoiler');
+            this.spoilerContent.style.height = this.paragraph.clientHeight + 'px';
+        } else {
+            this.spoilerContainer.classList.remove('show-spoiler');
+            this.spoilerContent.style.height = 0;
+        }
     }
 }
 
