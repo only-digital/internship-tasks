d4033ea81f3a864c4c6e62b78cd0586368907e78
Eugen Ermolaev <ermolaevjeka7591@gmail.com>
2023-10-24 11:31:07 +0300

    удалил компонент example-button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,10 @@
 import styles from '../styles/index.module.scss'
-import ExampleButton from "../components/example-button/example-button";
 import {getIndexPage} from "../../lib/api";
 import Tasks from '@/components/tasks/tasks';
 
 function Index(props) {
-
-    // console.log(props)
     return (
         <main className={styles.main}>
-            {/* <ExampleButton/>
-            <ExampleButton initialValue={10}/> */}
-
             {<Tasks {...props}/>}
         </main>
     )
