fb167b0a14e7ff0d2ff5bf8c2d71dd18f48ea1b2
VictorM <neuron96@mail.ru>
2023-10-25 15:07:54 +0400

    feat: static page w/o form

---
+++ src/components/main-title/main-title.js
@@ -0,0 +1,13 @@
+import Component from "../../app/js/base/Component";
+
+class MainTitle extends Component {
+  constructor(element) {
+    super(element);
+
+    // Your code here
+  }
+
+  // Your code here
+}
+
+export default MainTitle;
---
+++ src/components/my-list/my-list.js
@@ -0,0 +1,13 @@
+import Component from '../../app/js/base/Component';
+
+class MyList extends Component {
+    constructor(element) {
+        super(element);
+
+        // Your code here
+    }
+
+    // Your code here
+}
+
+export default MyList
