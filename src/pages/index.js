33def32054e1d5867fb5847d1d63e19e192a771d
eazaykova <lena.zaykova.95@gmail.com>
2023-10-30 17:13:52 +0700

    Modified index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,30 @@
-import styles from '../styles/index.module.scss'
-import ExampleButton from "../components/example-button/example-button";
-import {getIndexPage} from "../../lib/api";
+import styles from "../styles/index.module.scss";
+import { getIndexPage } from "../../lib/api";
+import Header from "@/components/header/header";
+import Aside from "@/components/aside/aside";
+import TaskList from "@/components/task-list/task-list";
 
 function Index(props) {
+  console.log(props);
 
-    console.log(props)
-
-    return (
-        <main className={styles.main}>
-            <ExampleButton/>
-
-            <ExampleButton initialValue={10}/>
-
-            {/* Your code here */}
-
-        </main>
-    )
+  return (
+    <>
+      <Header />
+      <main className={styles.main}>
+        <Aside title={props.title} />
+        <TaskList title={props.title} tasks={props.tasks} />
+      </main>
+    </>
+  );
 }
 
 export const getStaticProps = async () => {
-    const indexPage = await getIndexPage();
+  const indexPage = await getIndexPage();
 
-    return {
-        props: indexPage,
-        revalidate: 1
-    };
+  return {
+    props: indexPage,
+    revalidate: 1,
+  };
 };
 
 export default Index;
