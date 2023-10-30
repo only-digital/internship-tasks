5078bf0599f4625b5a0263fddfe6de7aeaf66bdc
Whittun <whittun@gmail.com>
2023-10-30 16:37:04 +0300

    feat: add states and validation of the email field

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,13 +1,77 @@
 import Component from '../../app/js/base/component';
 
 class Form extends Component {
+    inputs;
+    emailErrorText;
+    tooltips;
+
     constructor(element) {
         super(element);
 
-        // Your code here
+        this.emailErrorText = this.getElement('email-error-text')
+        this.inputs = this.root.querySelectorAll('.form__input')
+        this.tooltips = this.root.querySelectorAll('.form__input-text')
+
+        this.inputs.forEach(input => {
+            input.addEventListener('change', this.changeHandler)
+            input.addEventListener('input', this.inputHandler)
+        })
     }
 
-    // Your code here
+    changeHandler = ({ target }) => {
+        if (target.hasAttribute('data-reg')) {
+            this.regCheck(target)
+        }
+    }
+
+    inputHandler = ( { target } ) => {
+        this.clearStatus(target)
+        this.showTooltip(target)
+        this.limitInput(target)
+    }
+
+    regCheck = (input) => {
+        const inputValue = input.value
+        const inputReg = input.getAttribute('data-reg')
+        const reg = new RegExp(inputReg)
+        
+        if (inputValue !== '') {
+            if (!reg.test(inputValue)) {
+                this.showError(input)
+            } else {
+                this.showSuccess(input)
+            }
+        }
+    }
+
+    showTooltip = (input) => {
+        if (input.value) {
+            input.nextSibling.classList.add('form__input-text--shown')
+        } else {
+            input.nextSibling.classList.remove('form__input-text--shown')
+        }
+    }
+    
+    showError = (input) => {
+        this.emailErrorText.textContent = 'Неправильный формат'
+        input.classList.add('input--error')
+    }
+
+    clearStatus = (input) => {
+        this.emailErrorText.textContent = ''
+        input.classList.remove('input--error')
+        input.classList.remove('input--success')
+    }
+
+    showSuccess = (input) => {
+        input.classList.add('input--success')
+    }
+
+    limitInput = (input) => {
+        if (input.value.length >= 255) {
+            input.value = input.value.substr(0, 255)
+        }
+    }
 }
 
 export default Form
