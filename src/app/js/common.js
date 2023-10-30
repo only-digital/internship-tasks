8396b338fdce53c5d525d5d30c3d0fc760bf67c3
Dmitriy Zakharov <dzaharov794@gmail.com>
2023-10-30 16:27:14 +0300

    add custom checkbox

--- src/app/js/common.js
+++ src/app/js/common.js
@@ -1,4 +1,4 @@
-import ExampleButton from "../../components/example-button/example-button";
+import ExampleButton from '../../components/example-button/example-button';
 
 /**
  * Объект со всеми компонентами, для которых будет применяться автоматическая инициализация
@@ -6,25 +6,29 @@ import ExampleButton from "../../components/example-button/example-button";
  * Значение - JS-класс компонента (Импорт добавляется вручную)
  */
 const allComponents = {
-    'example-button': ExampleButton,
-}
+  'example-button': ExampleButton,
+};
 
 /**
  * Инициализация всех компонентов на странице
  */
 try {
-    const existedComponents = Array.from(document.querySelectorAll('[data-component]'));
+  const existedComponents = Array.from(
+    document.querySelectorAll('[data-component]')
+  );
 
-    const components = existedComponents.map((component) => {
-        try {
-            return new allComponents[component.dataset.component]({
-                name: component.dataset.component,
-                component: component,
-            });
-        } catch (e) {
-            console.error(`Ошибка во время инициализации компонента: ${component.dataset.component}\n\n${e}`);
-        }
-    });
+  const components = existedComponents.map((component) => {
+    try {
+      return new allComponents[component.dataset.component]({
+        name: component.dataset.component,
+        component: component,
+      });
+    } catch (e) {
+      console.error(
+        `Ошибка во время инициализации компонента: ${component.dataset.component}\n\n${e}`
+      );
+    }
+  });
 } catch (e) {
-    console.error(e);
+  console.error(e);
 }
