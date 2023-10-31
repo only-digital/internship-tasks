8350b5ef298277532c6df58a140230a3b7702cda
Milana <milanakhairullina@gmail.com>
2023-10-31 21:11:46 +0300

    add: add completed tasks

--- src/components/todolist/todolist.jsx
+++ src/components/todolist/todolist.jsx
@@ -1,7 +1,7 @@
 
 const Todolist = (props) => {
 
-console.log("data", props, props.data)
+console.log("data", props.tasks)
 
     return (
         <div className={"todolist__container"}>
@@ -19,14 +19,19 @@ console.log("data", props, props.data)
                     </div>
                 </aside>
                 <div className="todolist__main">
-                    <h1>{props.data.title}</h1>
+                    <h1>{props.title}</h1>
                     <ul className="todolist__list">
                         {
-                            props.data.tasks.map((t) => {
+                            props.tasks.map((t, index) => {
+
+                                const taskHandler = () => {
+                                    props.changeTaskStatus(t.id);
+                                }
+
                                 return (
-                                <li className={"todolist__task"}>
-                                    <div className={"todolist__task-header"} className={t.isCompleted ? "isCompleted" : ""}>
-                                        <p className={"todolist__task-name"}>{t.title}</p>
+                                <li key={t.id} className={"todolist__task"} onClick={taskHandler}>
+                                    <div className={"todolist__task-header"}>
+                                        <p className={`todolist__task-name ${t.isCompleted ? "isCompleted" : ""}`}>{t.title}</p>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26" fill="none">
                                             <path d="M20 6L6 20" stroke="#007FFF" stroke-width="2" stroke-linecap="round"/>
                                             <path d="M6 6L20 20" stroke="#007FFF" stroke-width="2" stroke-linecap="round"/>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,41 @@
+import React, {useState} from 'react';
 import styles from '../styles/index.module.scss'
 import {getIndexPage} from "../../lib/api";
 import Todolist from "../components/todolist/todolist";
+import {v1} from "uuid";
 
 function Index(props) {
 
     console.log("here",props)
 
-    let initData = [...props.tasks]
+    const [initData, changeTasks] = useState([...props.tasks]);
+
+    initData.map((task) => {
+        task.id = v1();
+    })
+
+    [initData, changeTasks] = useState();
+
     console.log("init",initData)
+
+    function changeTaskStatus(id) {
+        initData.map((task) => {
+            if (task.id === id) {
+                task.isCompleted = !task.isCompleted;
+                changeTasks([...initData]);
+                return;
+            }
+        })
+        alert(initData[0].isCompleted)
+    }
 
     return (
         <main className={styles.main}>
 
             <Todolist
-                data={props}
+                title={props.title}
+                tasks={initData}
+                changeTaskStatus={changeTaskStatus}
             >
 
             </Todolist>
