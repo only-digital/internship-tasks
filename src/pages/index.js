507c669f56001d1923b7024ee4dd412c5b8b676c
katerina-shche <asti.katerina@gmail.com>
2023-11-04 22:03:15 +0100

    TaskList TaskCard DeleteButton + styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,25 +2,36 @@ import styles from '../styles/index.module.scss'
 import ExampleButton from "../components/example-button/example-button";
 import {getIndexPage} from "../../lib/api";
 import Navbar from '../components/navbar/navbar';
+import DeleteButton from '../components/delete-button/delete-button';
 
 
 function Index(props) {
 
     console.log(props)
-    const listName = props.title;
+    
     return (
-        <>
-        <Navbar listName={listName}/>
-        <p>{}</p>
-        <main className={styles.main}>
-            <ExampleButton/>
+        <div className={styles.Wrapper}>
+            <Navbar listName={props.title}/>
+            <div className={styles.TasksList}>
+                <h1>{props.title}</h1>
+                <ul>
+                {props.tasks.map((task, index) => (
+                    <li key={index} id={`taskcard` + index} className={styles.TaskCard + (task.isCompleted ? ' ' + styles.Completed : ' ' + styles.NotCompleted)}>
+                    <h2>{task.title}</h2><DeleteButton />
+                    <hr></hr>
+                    <p>{task.text}</p>
+                    </li>
+                ))}
+                </ul>
+            </div>
 
-            <ExampleButton initialValue={10}/>
+        </div>
 
-            {/* Your code here */}
+    
+        
+        
 
-        </main>
-        </>
+       
     )
 }
 
