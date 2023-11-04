ccac82e9ce54d86106413ad68f277c85307d5069
Nastya <nkartsel@gmail.com>
2023-11-04 12:02:56 +0300

    feat: add index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,23 @@ import styles from '../styles/index.module.scss'
 import ExampleButton from "../components/example-button/example-button";
 import {getIndexPage} from "../../lib/api";
 
+import Aside from '@/components/aside/aside';
+import Header from '@/components/header/header';
+import Content from '@/components/content/content';
+
 function Index(props) {
+
+    
 
     console.log(props)
 
     return (
         <main className={styles.main}>
-            <ExampleButton/>
-
-            <ExampleButton initialValue={10}/>
-
-            {/* Your code here */}
-
+            <Header />
+            <div className={styles.container}>
+                <Aside />
+                <Content props={props} />
+            </div>
         </main>
     )
 }
