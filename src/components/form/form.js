fcc84516871359ed6f1daf41c8164e7cd83e58dc
Whittun <whittun@gmail.com>
2023-11-06 10:50:46 +0300

    feat: add field validation

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -2,32 +2,61 @@ import Component from '../../app/js/base/component';
 
 class Form extends Component {
     inputs;
+    fileInput;
+    fileSection;
+    messageInput;
+    messageHiddenClone;
+    emailInput;
     emailErrorText;
     tooltips;
+    fileWrap;
+    fileTextError;
 
     constructor(element) {
         super(element);
 
         this.emailErrorText = this.getElement('email-error-text')
         this.inputs = this.root.querySelectorAll('.form__input')
+        this.messageInput = this.getElement('message-input')
+        this.messageHiddenClone = this.getElement('message-hidden-clone')
+        this.emailInput = this.getElement('email-input')
         this.tooltips = this.root.querySelectorAll('.form__input-text')
+        this.fileInput = this.getElement('file-hidden-input')
+        this.fileSection = this.getElement('file-wrap')
+        this.fileWrap = this.getElement('file-output')
+        this.fileTextError = this.getElement('file-text-error')
 
         this.inputs.forEach(input => {
-            input.addEventListener('change', this.changeHandler)
-            input.addEventListener('input', this.inputHandler)
+            input.addEventListener('change', this.fieldChangeHandler)
+            input.addEventListener('input', this.fieldInputHandler)
         })
+
+        this.messageInput.addEventListener('input', this.autoHeight)
+        this.fileInput.addEventListener('change', this.fileChangeHandler)
     }
 
-    changeHandler = ({ target }) => {
+    fileChangeHandler = ({ target }) => {
+        this.fileCheck()
+        this.renderFiles(target)
+    }
+
+    fieldChangeHandler = ({ target }) => {
         if (target.hasAttribute('data-reg')) {
             this.regCheck(target)
+        } else {
+            this.messageCheck(target)
         }
     }
 
-    inputHandler = ( { target } ) => {
+    fieldInputHandler = ( { target } ) => {
         this.clearStatus(target)
         this.showTooltip(target)
-        this.limitInput(target)
+
+        if (target.hasAttribute('data-reg')) {
+            this.limitInput(target, 255)
+        } else {
+            this.limitInput(target, 1000)
+        }
     }
 
     regCheck = (input) => {
@@ -37,10 +66,20 @@ class Form extends Component {
         
         if (inputValue !== '') {
             if (!reg.test(inputValue)) {
-                this.showError(input)
+                this.showError(input, 'Неправильный формат')
             } else {
                 this.showSuccess(input)
             }
+        } else {
+            this.showError(input, 'Поле обязательно')
+        }
+    }
+
+    messageCheck = (input) => {
+        if (input.value) {
+            this.showSuccess(input)
+        } else {
+            this.showError(input, 'Поле обязательно')
         }
     }
 
@@ -52,26 +91,144 @@ class Form extends Component {
         }
     }
     
-    showError = (input) => {
-        this.emailErrorText.textContent = 'Неправильный формат'
+    showError = (input, text) => {
+        const errorText = input.previousElementSibling
+
         input.classList.add('input--error')
+        errorText.textContent = text
     }
 
     clearStatus = (input) => {
-        this.emailErrorText.textContent = ''
+        const errorText = input.previousElementSibling
+
         input.classList.remove('input--error')
         input.classList.remove('input--success')
+
+        errorText.textContent = ''
     }
 
     showSuccess = (input) => {
         input.classList.add('input--success')
     }
 
-    limitInput = (input) => {
-        if (input.value.length >= 255) {
-            input.value = input.value.substr(0, 255)
+    limitInput = (input, limit) => {
+        if (input.value.length >= limit) {
+            input.value = input.value.substr(0, limit)
         }
     }
-}
+
+    autoHeight = ({ target }) => {
+        this.messageHiddenClone.textContent = this.messageInput.value
+
+        if (this.messageHiddenClone.offsetHeight > 191) {
+            target.style.height = this.messageHiddenClone.offsetHeight + 'px' 
+        } else {
+            target.style.height = '191px'
+        } 
+
+        if (!target.value) {
+            target.removeAttribute('style')
+        }
+    }
+
+    createFile = (textValue, sizeValue, index) => {
+        const file = document.createElement('div')
+        const textWrapper = document.createElement('div')
+        const text = document.createElement('p')
+        const dataWrapper = document.createElement('div')
+        const dataType = document.createElement('p')
+        const dataSize = document.createElement('p')
+        const button = document.createElement('button')
+        
+        file.addEventListener('click', this.removeFile)
+
+        file.classList.add('form__file-elem')   
+        file.setAttribute('index', index)
+        textWrapper.classList.add('form__file-text-wrap')   
+        text.classList.add('form__file-text')   
+        dataWrapper.classList.add('form__file-data-wrap')   
+        dataType.classList.add('form__file-data-type')   
+        dataSize.classList.add('form__file-data-size')  
+        button.classList.add('form__file-button')
+
+        file.append(textWrapper)
+        file.append(dataWrapper)
+        file.append(button)
+
+        textWrapper.append(text)
+        dataWrapper.append(dataType)
+        dataWrapper.append(dataSize)
+
+        let typeValue; 
+        [textValue, typeValue] = textValue.split('.')
+
+        text.textContent = textValue.slice(0, 13) + '...'
+        dataType.textContent = typeValue.toUpperCase() + ','
+        dataSize.textContent = Math.ceil(sizeValue / 1024) + ' kB'
+
+        return file
+    }
+
+    removeFile = ({ target }) => {
+        const files = this.fileInput.files
+        
+        const indexToRemove = +target.getAttribute('index')
+        console.log(indexToRemove)
+
+        const newFiles = []
+
+        for (var i = 0; i < files.length; i++) {
+            if (i !== indexToRemove) {
+                newFiles.push(files[i])
+            }
+        }
+
+        this.fileInput.value = ''
+
+        const dataTransfer = new DataTransfer();
+        
+        for (var i = 0; i < newFiles.length; i++) {
+            dataTransfer.items.add(newFiles[i]);
+        }
+
+        this.fileInput.files = dataTransfer.files;
+        console.log(this.fileInput.files)
+        this.renderFiles(this.fileInput)
+    }
+
+    renderFiles = (input) => {   
+        this.fileWrap.innerHTML = ''
+
+        const files = Array.from(input.files)
+
+        files.forEach((fileElem, index) => {
+            const file = this.createFile(fileElem.name, fileElem.size, index)
+            this.fileWrap.append(file)
+        })  
+
+        this.fileSection.append(this.fileWrap)
+    }
+
+    fileCheck = () => {
+        this.fileTextError.textContent = ''
+
+        const files = Array.from(this.fileInput.files)
+
+        files.forEach((file) => {
+            const size = Math.ceil(file.size / 1024)
+            const maxSize = 5000
+            if (size > maxSize) {
+                this.fileTextError.textContent = 'Максимальный размер файла 5 MB'
+                this.fileInput.value = ''
+                return
+            }
+        })
+
+        if (files.length > 2) {
+            this.fileInput.value = ''
+            this.fileTextError.textContent = 'Можно добавить до двух файлов'
+        }
+    }
+}   
 
 export default Form
