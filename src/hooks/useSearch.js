5d90e982f246caad84e7ce609a51f3901f229096
Amin Hesenov <lolotron022gmail.com>
2023-11-07 18:56:51 +0400

    updated useSearch

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -3,11 +3,18 @@ import { useContext } from "react";
 
 export const useSearch = (search) => {
     const { activeTaskList } = useContext(TaskContext);
+    const loweredSearch = search.toLowerCase();
+
+    if (loweredSearch !== "")
+        return activeTaskList.tasks
+            .filter(task => {
+                const loweredTaskTitle = task.title.toLowerCase();
+                const loweredTaskText = task.text.toLowerCase();
+                return (
+                    loweredTaskTitle.includes(loweredSearch) ||
+                    loweredTaskText.includes(loweredSearch)
+                )
+            })
+
     return activeTaskList.tasks
-        .filter(task =>
-            (
-                task.title.includes(search) ||
-                task.text.includes(search)
-            ) && search !== ""
-        )
 }
