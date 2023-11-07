c83c1e7edd10074d3b374bcb5c012976fc8ac333
katerina-shche <asti.katerina@gmail.com>
2023-11-07 18:58:02 +0100

    Complited onCardClick

--- src/components/delete-button/delete-button.jsx
+++ src/components/delete-button/delete-button.jsx
@@ -3,14 +3,18 @@ import styles from './delete-button.module.scss'
 
 export default function DeleteButton(props) {
 
-    const onDelete = (e) => {
-        console.log(e.target.closest('.styles_TaskCard__uqRRn').id);
+    const handleButtonClick = (e, props) => {
+        //delete TaskCard 
+        e.stopPropagation();
+       let index = e.target.closest('.styles_TaskCard__uqRRn').id;
+        props.tasks.splice(id, 1);
+        console.log(props);
         e.target.closest('.styles_TaskCard__uqRRn').remove();
 
     }
 
   return (
-    <button className={styles.DeleteButton} onClick={onDelete}>
+    <button className={styles.DeleteButton} onClick={handleButtonClick}>
         <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26" fill="none">
             <path d="M20 6L6 20" stroke="#007FFF" strokeWidth="2" strokeLinecap="round"/>
             <path d="M6 6L20 20" stroke="#007FFF" strokeWidth="2" strokeLinecap="round"/>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,31 @@ import ExampleButton from "../components/example-button/example-button";
 import {getIndexPage} from "../../lib/api";
 import Navbar from '../components/navbar/navbar';
 import DeleteButton from '../components/delete-button/delete-button';
+import { useState } from 'react';
 
 
 function Index(props) {
+    console.log(props);
+    let arr = [];
+    const [isInitial, setIsInitial] = useState(Array.from({ length: props.tasks.length }, () => true))
+    console.log(isInitial);
+    console.log(props);
 
-    console.log(props)
+    const [myProps, setMyProps] = useState(props);
+
+    const onCardClick = (e) => {
+        if (e.target.closest('.styles_TaskCard__uqRRn').classList.contains('styles_Completed__rzorv')) {
+            e.target.closest('.styles_TaskCard__uqRRn').classList.remove('styles_Completed__rzorv');
+        } else {
+            e.target.closest('.styles_TaskCard__uqRRn').classList.add('styles_Completed__rzorv');
+        }
+        
+        // let cardId = e.target.closest('.styles_TaskCard__uqRRn').id;
+        // let myArr = isInitial;
+        // myArr[cardId] = !myArr[cardId];
+        // setIsInitial(myArr);
+        // console.log(isInitial);
+    }
     
     return (
         <div className={styles.Wrapper}>
@@ -16,8 +36,8 @@ function Index(props) {
                 <h1>{props.title}</h1>
                 <ul>
                 {props.tasks.map((task, index) => (
-                    <li key={index} id={`taskcard` + index} className={styles.TaskCard + (task.isCompleted ? ' ' + styles.Completed : ' ' + styles.NotCompleted)}>
-                    <h2>{task.title}</h2><DeleteButton />
+                    <li key={index} id={index} className={styles.TaskCard + (task.isCompleted ? ' ' + styles.Completed : '')} onClick={onCardClick}>
+                    <h2>{task.title}</h2><DeleteButton props={props}/>
                     <hr></hr>
                     <p>{task.text}</p>
                     </li>
