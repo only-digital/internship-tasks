2a36dac79439d1f782b9b4bdb27a89774a4876fe
Your Name <iraqiProfessionalDesigner@gmail.com>
2023-11-10 15:05:57 +0700

    task 4

--- src/app/js/common.js
+++ src/app/js/common.js
@@ -1,4 +1,12 @@
 import ExampleButton from "../../components/example-button/example-button";
+import Title from "../../components/title/title";
+import Description from "../../components/description/description";
+import Feedback from "../../components/feedback/feedback";
+import FormButton from "../../components/form-button/form-button";
+import FormResponse from "../../components/form-response/form-response";
+import InputCheckbox from "../../components/input-checkbox/input-checkbox";
+import InputText from "../../components/input-text/input-text";
+import Loader from "../../components/loader/loader";
 
 /**
  * Объект со всеми компонентами, для которых будет применяться автоматическая инициализация
@@ -6,25 +14,37 @@ import ExampleButton from "../../components/example-button/example-button";
  * Значение - JS-класс компонента (Импорт добавляется вручную)
  */
 const allComponents = {
-    'example-button': ExampleButton,
-}
+  "example-button": ExampleButton,
+  title: Title,
+  description: Description,
+  feedback: Feedback,
+  "form-button": FormButton,
+  "form-response": FormResponse,
+  "input-checkbox": InputCheckbox,
+  "input-text": InputText,
+  loader: Loader,
+};
 
 /**
  * Инициализация всех компонентов на странице
  */
 try {
-    const existedComponents = Array.from(document.querySelectorAll('[data-component]'));
+  const existedComponents = Array.from(
+    document.querySelectorAll("[data-component]")
+  );
 
-    const components = existedComponents.map((component) => {
-        try {
-            return new allComponents[component.dataset.component]({
-                name: component.dataset.component,
-                component: component,
-            });
-        } catch (e) {
-            console.error(`Ошибка во время инициализации компонента: ${component.dataset.component}\n\n${e}`);
-        }
-    });
+  const components = existedComponents.map((component) => {
+    try {
+      return new allComponents[component.dataset.component]({
+        name: component.dataset.component,
+        component: component,
+      });
+    } catch (e) {
+      console.error(
+        `Ошибка во время инициализации компонента: ${component.dataset.component}\n\n${e}`
+      );
+    }
+  });
 } catch (e) {
-    console.error(e);
+  console.error(e);
 }
