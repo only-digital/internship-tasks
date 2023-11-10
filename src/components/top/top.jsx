35046ea5e25aa511064b656c2124c39fea284b7e
Whittun <whittun@gmail.com>
2023-11-10 10:35:23 +0300

    feat: rename components

---
+++ src/components/top/top.jsx
@@ -0,0 +1,11 @@
+import styled from './top.module.scss';
+
+const Top = () => {
+    return (
+        <div className={styled.Top}>
+            {/* Your code here */}
+        </div>
+    )
+}
+
+export default Top;
---
+++ src/components/workingArea/workingArea.jsx
@@ -0,0 +1,11 @@
+import styled from './workingArea.module.scss';
+
+const WorkingArea = () => {
+    return (
+        <div className={styled.WorkingArea}>
+            {/* Your code here */}
+        </div>
+    )
+}
+
+export default WorkingArea;
