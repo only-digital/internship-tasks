5c29aa37e7eeb523c180037abd5408de96b6e80e
katerina-shche <asti.katerina@gmail.com>
2023-11-10 13:34:14 +0100

    useTasksSearch custom hook

--- src/hooks/useTasksSearch.jsx
+++ src/hooks/useTasksSearch.jsx
@@ -1,20 +1,30 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 
-function useTaskSearch(tasks, searchString) {
+export const useTasksSearch = (tasks, searchString) => {
+  
   const filteredTasks = useMemo(() => {
     if (!searchString) {
-      return tasks;
+      return null;
+    } else {
+      const lowercaseSearch = searchString.toLowerCase();
+      const filtered = tasks.filter((task) => {
+        const lowercaseTitle = task.title.toLowerCase();
+        const lowercaseText = task.text.toLowerCase();
+        return lowercaseTitle.includes(lowercaseSearch) || lowercaseText.includes(lowercaseSearch);
+      });
+      return filtered;
     }
-
-    const lowercaseSearch = searchString.toLowerCase();
-    return tasks.filter((task) => {
-      const lowercaseTitle = task.title.toLowerCase();
-      const lowercaseText = task.text.toLowerCase();
-      return lowercaseTitle.includes(lowercaseSearch) || lowercaseText.includes(lowercaseSearch);
-    });
   }, [tasks, searchString]);
 
-  return filteredTasks;
-}
+  
+  // Return the memoized value directly
+  return { filteredTasks };
+};
 
-export default useTaskSearch;
+
+
+
+
+
+
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,26 @@ import {getIndexPage} from "../../lib/api";
 import Navbar from '../components/navbar/navbar';
 import DeleteButton from '../components/delete-button/delete-button';
 import { useState } from 'react';
+import { useTasksSearch } from '../../src/hooks/useTasksSearch';
+import { v4 as uuidv4 } from 'uuid';
 
 
 
 function Index(props) {
 
     const [data, setData] = useState(props);
+    console.log(data);
+    const [searchStr, setSearchStr] = useState(false);
+   
+    const { filteredTasks } = useTasksSearch([...data.tasks], searchStr);
+    console.log(filteredTasks);
 
     const handleButtonClick = (e) => {
         e.stopPropagation();
-        let taskIndex = e.target.closest('.styles_TaskCard__uqRRn').id;
+        let elemId = e.target.closest('.styles_TaskCard__uqRRn').id;
+        let taskIndex = data.tasks.findIndex(item => item.id === elemId);
+        console.log(data.tasks);
+        console.log(elemId, taskIndex);
         // copy of the data object to avoid mutating the state directly
         let newData = { ... data };
         let newTasks = [... data.tasks];
@@ -25,10 +35,9 @@ function Index(props) {
     }
 
     const onCardClick = (e) => {
-        console.log(e.target);
         e.stopPropagation();
-        
-        let taskIndex = e.target.closest('.styles_TaskCard__uqRRn').id;
+        let elemId = e.target.closest('.styles_TaskCard__uqRRn').id;
+        let taskIndex = data.tasks.findIndex(item => item.id === elemId);
         // copy of the data object to avoid mutating the state directly
         let newData = { ... data };
         let newTasks = [... data.tasks];
@@ -41,16 +50,33 @@ function Index(props) {
         // set data
         setData(newData);
     }
-    
+    const handleInput = (e) => {
+        setSearchStr(e.target.value);
+    }
     
     return (
         <div className={styles.Wrapper}>
             <Navbar listName={data.title}/>
             <div className={styles.TasksList}>
+                <label htmlFor='search'>
+                    <input type="search" name='search' id='search' placeholder="Поиск" onInput={handleInput}/>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <circle cx="11" cy="11" r="6.25" stroke="#007FFF" strokeWidth="1.5"/>
+                        <path d="M15.9058 16.021L20.1944 19.697" stroke="#007FFF" strokeWidth="1.5" strokeLinecap="round"/>
+                    </svg>
+                </label>
+                
                 <h1>{data.title}</h1>
                 <ul>
-                {data.tasks.map((task, index) => (
-                    <li key={index} id={index} className={styles.TaskCard + (task.isCompleted ? ' ' + styles.Completed : '')} onClick={onCardClick}>
+                {filteredTasks && filteredTasks.map((task, index) => (
+                    <li key={index} id={task.id} className={styles.TaskCard + (task.isCompleted ? ' ' + styles.Completed : '')} onClick={onCardClick}>
+                    <h2>{task.title}</h2><DeleteButton handleButtonClick={handleButtonClick}/>
+                    <hr></hr>
+                    <p>{task.text}</p>
+                    </li>
+                ))}   
+                {!filteredTasks && data.tasks.map((task, index) => (
+                    <li key={index} id={task.id} className={styles.TaskCard + (task.isCompleted ? ' ' + styles.Completed : '')} onClick={onCardClick}>
                     <h2>{task.title}</h2><DeleteButton handleButtonClick={handleButtonClick}/>
                     <hr></hr>
                     <p>{task.text}</p>
