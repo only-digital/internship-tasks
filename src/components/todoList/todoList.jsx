ae8936a76acbaa5f528f30d3b06a2685aedd1404
Whittun <whittun@gmail.com>
2023-11-11 09:11:57 +0300

    feat: add html

--- src/components/todoList/todoList.jsx
+++ src/components/todoList/todoList.jsx
@@ -6,6 +6,9 @@ const TodoList = () => {
             <h2 className={styled.TodoList__title}>
                 Список задач
             </h2>
+            <ul className={styled.TodoList__list}>
+                
+            </ul>
         </section>
     )
 }
