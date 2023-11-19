d9e44eb5794de38339df6b940341ac03b8a9541c
Dmitriy Zakharov <dzaharov794@gmail.com>
2023-11-19 23:25:18 +0300

    add form component

--- src/app/js/common.js
+++ src/app/js/common.js
@@ -1,4 +1,6 @@
 import ExampleButton from "../../components/example-button/example-button";
+import Feedback from "../../components/feedback/feedback";
+import Button from "../../components/button/button";
 
 /**
  * Объект со всеми компонентами, для которых будет применяться автоматическая инициализация
@@ -7,6 +9,8 @@ import ExampleButton from "../../components/example-button/example-button";
  */
 const allComponents = {
     'example-button': ExampleButton,
+    'feedback': Feedback,
+    'button': Button,
 }
 
 /**
