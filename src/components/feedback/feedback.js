75465f9f8b30f0d6c662ebddae94a8a07a758164
Your Name <iraqiProfessionalDesigner@gmail.com>
2023-12-30 13:30:21 +0700

    fixed according to comments

--- src/components/feedback/feedback.js
+++ src/components/feedback/feedback.js
@@ -3,7 +3,7 @@ import Component from "../../app/js/base/component";
 class Feedback extends Component {
     feedBackForm;
     submitButton;
-    emailError;
+    errors;
     formResponse;
     formLoader;
 
@@ -12,21 +12,23 @@ class Feedback extends Component {
 
         this.feedBackForm = this.getElement("form");
         this.submitButton = this.getElement("form-button");
-        this.emailError = this.feedBackForm.querySelector(".input-text__error");
+        this.errors = this.feedBackForm.querySelector(".input-text__error");
         this.formResponse = this.feedBackForm.querySelector(".form-response__text");
         this.formLoader = this.getElement("form-loader");
-
         this.submitButton.addEventListener("click", this.sendForm);
     }
-
+    clearErrors (){
+        this.errors.textContent = "";
+    }
     sendForm = async (e) => {
         e.preventDefault();
-        const emailValue = document.getElementById("email").value;
-        const isConfirm = document.getElementById("confirm").checked;
-
-        this.formLoader.style.display = "flex";
+        this.clearErrors();
+        const emailValue = document.querySelector("form.feedback__form input#email").value;
+        const isConfirm = document.querySelector("form.feedback__form input#confirm").checked;
+        this.formLoader.classList.add("show");
+        
         try {
-
+            
             let response = await fetch("/form", {
                 method: "POST",
                 headers: {
@@ -39,20 +41,21 @@ class Feedback extends Component {
             });
 
             let result = await response.json();
-            this.formLoader.style.display = "none";
 
             if (response.status === 200) {
                 this.feedBackForm.classList.add("sent");
-                this.emailError.textContent = "";
                 this.formResponse.textContent = result.message;
             } else if (response.status === 422) {
-                this.emailError.textContent = result.message;
+                this.errors.textContent = result.message;
             }
-            
+
         } catch (e) {
             console.log(e)
         }
-
+        finally {    
+            this.formLoader.classList.remove("show");
+        }
+        
     };
 }
 
