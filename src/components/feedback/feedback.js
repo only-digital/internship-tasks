f220850e6f00040c75872d9205febf5be4340465
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-01-08 23:35:19 +0300

    add states for feedback policy

--- src/components/feedback/feedback.js
+++ src/components/feedback/feedback.js
@@ -1,13 +1,25 @@
 import Component from '../../app/js/base/component';
 
 class Feedback extends Component {
-    constructor(element) {
-        super(element);
+  constructor(element) {
+    super(element);
 
-        // Your code here
+    this.inputElement = this.getElement('input-policy');
+    this.labelElement = this.getElement('policy-label');
+    this.linkElement = this.getElement('policy-link');
+    this.updateLabel();
+    this.inputElement.addEventListener('change', () => this.updateLabel());
+  }
+
+  updateLabel() {
+    if (this.inputElement.disabled) {
+      this.labelElement.style.color = '#bcbcbc';
+      this.linkElement.style.color = '#bcbcbc';
+    } else {
+      this.labelElement.style.color = '';
+      this.linkElement.style.color = '';
     }
-
-    // Your code here
+  }
 }
 
-export default Feedback
+export default Feedback;
