a8bec03da5b5a98c521688ece4269128d1c197ef
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-01-11 21:53:40 +0300

    add validation for input-email component

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,13 +1,46 @@
 import Component from '../../app/js/base/component';
 
 class Input extends Component {
-    constructor(element) {
-        super(element);
+  inputEmail;
+  inputWrapper;
+  errorMessage;
+  constructor(element) {
+    super(element);
 
-        // Your code here
+    this.invalidEmail = false;
+    this.regEmail =
+      /^([a-zA-Z\-0-9_]+|([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)+)|(".+"))@(([a-zA-Z\-0-9]+\.)+[a-zA-Z\-0-9]{2,})$/;
+    this.MAX_LENGTH_EMAIL = 255;
+
+    this.inputEmail = document.querySelector('.input__email');
+    this.inputWrapper = document.querySelector('.input__wrapper');
+    this.errorMessage = document.querySelector('.input__email-error-message');
+
+    this.root.addEventListener('blur', this.validateEmail);
+    this.root.addEventListener('input', this.clearError);
+  }
+
+  validateEmail = () => {
+    const email = this.inputEmail.value;
+    if (email.length > this.MAX_LENGTH_EMAIL || !this.regEmail.test(email)) {
+      this.invalidEmail = true;
+      this.errorMessage.textContent = 'Некорректная почта';
+      this.inputWrapper.classList.add('input__wrapper--invalid');
+    } else {
+      this.invalidEmail = false;
+      this.errorMessage.textContent = '';
+      this.inputWrapper.classList.remove('input__wrapper--invalid');
+      this.inputWrapper.classList.add('input__wrapper--valid');
     }
+  };
 
-    // Your code here
+  clearError = () => {
+    console.log(this.invalidEmail)
+    if (this.invalidEmail) {
+      this.inputWrapper.classList.remove('input__wrapper--valid');
+      this.errorMessage.textContent = '';
+    }
+  }
 }
 
-export default Input
+export default Input;
