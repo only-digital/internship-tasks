e534821fc23a2ab31a507bfac4c101fe2d4f3a53
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-01-13 00:26:03 +0300

    add file upload

--- src/components/upload_btn/upload_btn.js
+++ src/components/upload_btn/upload_btn.js
@@ -1,13 +1,36 @@
 import Component from '../../app/js/base/component';
 
 class Upload_btn extends Component {
-    constructor(element) {
-        super(element);
+  inputUploadFile;
+  fileContainer;
+  cancelBtn;
+  fileInfo;
+  constructor(element) {
+    super(element);
 
-        // Your code here
-    }
+    this.inputUploadFile = document.querySelector('.upload_btn__upload-file');
+    this.fileContainer = document.querySelector('.upload_btn__file-container');
+    this.fileInfo = document.querySelector('.upload_btn__fileName');
+    this.cancelBtn = document.querySelector('.upload_btn__cancel');
 
-    // Your code here
+    this.root.addEventListener('change', this.onUploadFile);
+  }
+
+  onUploadFile = () => {
+    const file = this.inputUploadFile.files[0];
+    const fileExtension = file.type.split('/')[1].toUpperCase();
+    const fileSize = Math.round(file.size / 1024);
+    this.fileContainer.classList.add('upload_btn__file-container--active');
+    this.fileInfo.textContent = `Документ ${fileExtension}, ${fileSize} kB`;
+    this.cancelBtn.addEventListener('click', this.onCancel);
+  };
+
+  onCancel = () => {
+    this.fileContainer.classList.remove('upload_btn__file-container--active');
+    this.inputUploadFile.value = '';
+  }
+
+  // Your code here
 }
 
-export default Upload_btn
+export default Upload_btn;
