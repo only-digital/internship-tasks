fa0844fcf1c5770a54d4c57925780daf7ede5b46
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-01-14 22:06:15 +0300

    Add blocking of the submit button if one of the fields is not valid

--- src/components/feedback/feedback.js
+++ src/components/feedback/feedback.js
@@ -4,20 +4,34 @@ class Feedback extends Component {
   constructor(element) {
     super(element);
 
-    this.inputElement = this.getElement('input-policy');
+    this.form = this.getElement('form');
+    this.emailInput = document.querySelector('.input__email');
+    this.messageInput = document.querySelector('.textarea');
+    this.policyCheckbox = this.getElement('input-policy');
     this.labelElement = this.getElement('policy-label');
     this.linkElement = this.getElement('policy-link');
-    this.updateLabel();
-    this.inputElement.addEventListener('change', () => this.updateLabel());
+    this.submitBtn = document.querySelector('button[type="submit"]');
+    
+    this.policyCheckbox.addEventListener('change', this.updateLabel);
+    this.form.addEventListener('change', this.checkInputsValidity);
+    
   }
 
-  updateLabel() {
-    if (this.inputElement.disabled) {
+  updateLabel = () => {
+    if (this.policyCheckbox.disabled) {
       this.labelElement.style.color = '#bcbcbc';
       this.linkElement.style.color = '#bcbcbc';
     } else {
       this.labelElement.style.color = '';
       this.linkElement.style.color = '';
+    }
+  }
+
+  checkInputsValidity = () => {
+    if (this.emailInput.validity.valid && this.messageInput.validity.valid && this.policyCheckbox.checked) {
+      this.submitBtn.disabled = false;
+    } else {
+      this.submitBtn.disabled = true;
     }
   }
 }
