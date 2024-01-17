4f8c05a98f2fa93d0d361dd1a7923b877abb7b95
VictorM <neuron96@mail.ru>
2024-01-17 15:07:07 +0500

    fix: remove headers

--- src/components/my-form/my-form.js
+++ src/components/my-form/my-form.js
@@ -175,9 +175,6 @@ class MyForm extends Component {
       const res = await fetch(`${this.baseURL}/form`, {
         method: "POST",
         body: data,
-        headers: {
-          "Content-Type": "application/json",
-        },
       });
     } catch (error) {
       console.error("Ошибка:", error);
