53aa710e0816c6b4dc6b5f74f8f571b804f02382
VictorM <neuron96@mail.ru>
2024-01-28 18:09:28 +0500

    init: task-6

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,28 @@
-import styles from '../styles/index.module.scss'
+import styles from "../styles/index.module.scss";
 import ExampleButton from "../components/example-button/example-button";
-import {getIndexPage} from "../../lib/api";
+import { getIndexPage } from "../../lib/api";
 
 function Index(props) {
+  console.log(props);
 
-    console.log(props)
+  return (
+    <main className={styles.main}>
+      {/* <ExampleButton/>
 
-    return (
-        <main className={styles.main}>
-            <ExampleButton/>
+            <ExampleButton initialValue={10}/> */}
 
-            <ExampleButton initialValue={10}/>
-
-            {/* Your code here */}
-
-        </main>
-    )
+      {/* Your code here */}
+    </main>
+  );
 }
 
 export const getStaticProps = async () => {
-    const indexPage = await getIndexPage();
+  const indexPage = await getIndexPage();
 
-    return {
-        props: indexPage,
-        revalidate: 1
-    };
+  return {
+    props: indexPage,
+    revalidate: 1,
+  };
 };
 
 export default Index;
