07f44b17ef3c7c7b53e8a688ecab96a739b1225e
VictorM <neuron96@mail.ru>
2024-01-29 14:22:35 +0500

    feat: done main tasks

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,15 +1,24 @@
 import styled from "./TaskItem.module.scss";
 
-const TaskItem = ({ tasks }) => {
+const TaskItem = ({ tasks, onChange, onDelete }) => {
   return (
     <>
       {tasks.map((task) => (
         <div key={task.title.split(" ")[1]} className={styled.TaskItem}>
           <div className={styled.TaskItem__card}>
-            <div className={styled.TaskItem__header}>
-              {task.title}
-              <button>
-                <img src="cross.svg" alt="close" />
+            <div
+              className={`${styled.TaskItem__header}  ${
+                task.isCompleted ? styled.done : ""
+              }`}
+            >
+              <button
+                onClick={() => onChange(task.id)}
+                title="Нажмите чтобы изменить статус задачи"
+              >
+                {task.title}
+              </button>
+              <button onClick={() => onDelete(task.id)}>
+                <img src="cross.svg" alt="close" title="удалить" />
               </button>
             </div>
             <div className={styled.TaskItem__descr}>{task.text}</div>
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -4,12 +4,33 @@ import TaskItem from "../TaskItem/TaskItem";
 
 const TaskList = ({ tasks, title }) => {
   const [data, setData] = useState(tasks);
+
   console.log(data);
+
+  const handleDelete = (id) => {
+    setData((prev) => prev.filter((task) => task.id !== id));
+  };
+
+  const handleChangeStatus = (id) => {
+    const index = data.findIndex((task) => task.id === id);
+    const newArr = [...data];
+    newArr[index].isCompleted = !newArr[index].isCompleted;
+    setData(newArr);
+  };
+
   return (
     <>
       <h3 className={styled.TaskList__title}>{title}</h3>
       <div className={styled.TaskList__items}>
-        <TaskItem tasks={data} />
+        {data.length ? (
+          <TaskItem
+            tasks={data}
+            onChange={handleChangeStatus}
+            onDelete={handleDelete}
+          />
+        ) : (
+          "пуст..."
+        )}
       </div>
     </>
   );
