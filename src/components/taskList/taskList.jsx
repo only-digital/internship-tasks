2fc0350c916c7471484409d630b1391daf84cb53
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-01-30 23:32:37 +0300

    add style for task and taskList components

--- src/components/taskList/taskList.jsx
+++ src/components/taskList/taskList.jsx
@@ -1,20 +1,24 @@
+import { Fragment } from 'react';
 import styled from './taskList.module.scss';
 import Task from '../task/task';
 
 const TaskList = (props) => {
   return (
-    <ul className={styled.TaskList}>
-      {props.tasks.map((task) => {
-        return (
-          <Task
-            key={task.id}
-            id={task.id}
-            task={task}
-            onDelete={props.onDeleteTask}
-          />
-        );
-      })}
-    </ul>
+    <Fragment>
+      <h2 className={styled.TaskList__title}>{props.title}</h2>
+      <ul className={styled.TaskList}>
+        {props.tasks.map((task) => {
+          return (
+            <Task
+              key={task.id}
+              id={task.id}
+              task={task}
+              onDelete={props.onDeleteTask}
+            />
+          );
+        })}
+      </ul>
+    </Fragment>  
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,25 @@ import styles from '../styles/index.module.scss';
 import { getIndexPage } from '../../lib/api';
 import Header from '../components/header/header';
 import Sidebar from '@/components/sidebar/sidebar';
+import TaskList from '@/components/taskList/taskList';
 
 function Index(props) {
-  const sideItems = [{
-    id: 1,
-    text: props.title,
-  }]
+  const sideItems = [
+    {
+      id: 1,
+      text: props.title,
+    },
+  ];
 
   return (
     <Fragment>
       <Header />
-      <Sidebar items={sideItems} />
-      <main className={styles.main}>
-
-      </main>
+      <div className={styles.wrapper}>
+        <Sidebar items={sideItems} />
+        <main className={styles.main}>
+          <TaskList tasks={props.tasks} title={props.title}/>
+        </main>
+      </div>
     </Fragment>
   );
 }
