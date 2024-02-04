b2049006a003c589fb06e9320523028d93809ca4
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-02-04 22:52:47 +0300

    search query put in variable

--- src/components/hooks/use-search.js
+++ src/components/hooks/use-search.js
@@ -1,8 +1,9 @@
 const useSearch = (searchQuery, data) => {
+  let search = searchQuery.toLowerCase().trim();
   const searchResult = data.filter((item) => {
     return (
-      item.title.toLowerCase().includes(searchQuery.toLowerCase().trim()) ||
-      item.text.toLowerCase().includes(searchQuery.toLowerCase().trim())
+      item.title.toLowerCase().includes(search) ||
+      item.text.toLowerCase().includes(search)
     );
   });
 
