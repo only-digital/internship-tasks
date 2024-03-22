cb3597baa6ca2f2a4f168b0477e2abe902530f94
aleksnvkznsk <aleksandrov.developer@gmail.com>
2024-03-22 16:47:34 +0700

    change form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,53 +1,48 @@
-import Component from '../../app/js/base/Component';
+import Component from "../../app/js/base/Component";
 
 class Form extends Component {
-    constructor(element) {
-        super(element);
-
-        // Your code here
-        const button = this.getElement('content__button');
-        const clickForm = async (event) => {
-            event.preventDefault();
-
-            button.removeEventListener('click', clickForm);
-
-            const email = document.querySelector('.form__content__input').value;
-            const confirm = document.querySelector('.form__content__container__input').checked;
-            const data = { 
-                email: email,
-                confirm: confirm,
-            };
-        
-            console.log(data);
-        
-            try {
-                const response = await fetch('/form', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(data)
-                });
-        
-                if (response.ok) {
-                    document.querySelector('.form__content__container__message').textContent = 'Отправлено';
-                    document.querySelector('.form__content__container__message').classList.add('green');
-                } else {
-                    document.querySelector('.form__content__container__message').textContent = 'Ошибка';
-                    document.querySelector('.form__content__container__message').classList.add('red');
-                }
-            } catch (error) {
-                console.error('Ошибка получения данных(form)', error);
-            }
-        };
-        
-        button.addEventListener('click', clickForm);
-     
-    }
+  constructor(element) {
+    super(element);
 
     // Your code here
+    this.form = this.root.querySelector("form");
+    this.form.addEventListener("submit", this.submitForm);
+  }
 
-    
+  // Your code here
+  submitForm = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const data = {
+      email: formData.get("email"),
+      confirm: !!formData.get("confirm"),
+    };
+
+    try {
+        const response = await fetch('/form', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        console.log(e.target);
+        console.log(data);
+
+      if (response.ok) {
+        document.querySelector(".form__content__container__message").textContent = "Отправлено";
+        document.querySelector(".form__content__container__message").classList.add( "green" );
+      } else {
+        document.querySelector(".form__content__container__message").textContent = "Ошибка";
+        document.querySelector(".form__content__container__message").classList.add( "red" );
+      }
+    } catch (error) {
+      console.error("Ошибка получения данных(form)", error);
+    }
+  };
 }
 
-export default Form
+export default Form;
